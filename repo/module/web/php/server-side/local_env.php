8029e3aabac30e205dc28d8e14ad0819c19ff902
gfursin <Grigori.Fursin@cTuning.org>
2014-11-17 12:15:22 +0100

    Adding server side php

---
+++ repo/module/web/php/server-side/local_env.php
@@ -0,0 +1,15 @@
+<?php
+ /*
+
+  Collective Knowledge
+
+  See CK LICENSE.txt for licensing details.
+  See CK Copyright.txt for copyright details.
+
+  Developer: Grigori Fursin
+
+ */
+
+#putenv("CK_ROOT=/home1/fursinne/fggwork/ck");
+#putenv("CK_LOCAL_REPO=/home1/fursinne/fggwork/ck-repos/fgg-local-fursin.net");
+#putenv("CK_REPOS=/home1/fursinne/fggwork/ck-repos");
