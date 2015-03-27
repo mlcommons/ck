033f7b4494f06e51d9fd2ee64a5475d24a7d03e6
Grigori Fursin <grigori.fursin@cTuning.org>
2015-03-27 12:46:00 +0100

    Adding full update of CK as 'ck pull all'

---
+++ repo/module/all/module.py
@@ -0,0 +1,68 @@
+#
+# Collective Knowledge (some common operations for all CK)
+#
+# See CK LICENSE.txt for licensing details
+# See CK Copyright.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://cTuning.org/lab/people/gfursin
+#
+
+cfg={}  # Will be updated by CK (meta description of this module)
+work={} # Will be updated by CK (temporal data)
+ck=None # Will be updated by CK (initialized CK kernel) 
+
+# Local settings
+
+##############################################################################
+# Initialize module
+
+def init(i):
+    """
+
+    Input:  {}
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+    return {'return':0}
+
+##############################################################################
+# update CK and all repos
+
+def pull(i):
+    """
+    Input:  {
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+
+    import os
+
+    o=i.get('out','')
+
+    ck_root=ck.work['env_root']
+    os.chdir(ck_root)
+
+    if o=='con':
+       ck.out('Updating CK from GITHUB ...\n')
+
+    rx=os.system('git pull')
+    if rx>0: 
+       return {'return':1, 'error':'CK update failed'}
+
+    if o=='con':
+       ck.out('')
+
+    return ck.access({'action':'pull',
+                      'module_uoa':'repo',
+                      'out':o})
