f73c67effac4b4d1a3ed63a4537b4164318179ac
Grigori Fursin <Grigori.Fursin@cTuning.org>
2015-09-15 13:46:47 +0200

    fixing ck batch script for standard Python packaging

--- bin/ck.bat
+++ bin/ck.bat
@@ -17,10 +17,8 @@ set ck_path=%~dp0\..
 rem Check if CK_ROOT is defined and used it, otherwise use auto-detected path
 IF "%CK_ROOT%"=="" set CK_ROOT=%ck_path%
 
-rem Load kernel module
+rem Load kernel module (either GIT/local installation or as package)
 IF EXIST %CK_ROOT%\ck\kernel.py (
- python -W ignore::DeprecationWarning %CK_ROOT%\ck\kernel.py %*
-) ELSE IF EXIST LIB\site-packages\ck\kernel.py (
  python -W ignore::DeprecationWarning %CK_ROOT%\ck\kernel.py %*
 ) ELSE (
  python -W ignore::DeprecationWarning -m ck.kernel %*
