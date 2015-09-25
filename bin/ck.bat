af2c7dc244871dae5e4ed1ff82c2e084ba82e4df
Grigori Fursin <Grigori.Fursin@cTuning.org>
2015-09-25 12:14:10 +0200

    removing remarks from batch ck files

--- bin/ck.bat
+++ bin/ck.bat
@@ -1,16 +1,5 @@
 @echo off
 
-rem
-rem Collective Knowledge
-rem
-rem See CK LICENSE.txt for licensing details.
-rem See CK COPYRIGHT.txt for copyright details.
-rem
-rem Developer: Grigori Fursin
-rem
-
-rem CK entry point
-
 rem Set default path by detecting the path to this script
 set ck_path=%~dp0\..
 
