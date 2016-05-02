531b04ee30531c0937bf0d5772468c218e2e172c
Grigori Fursin <grigori.fursin@cTuning.org>
2016-05-02 17:48:13 +0200

    tagging new development version

--- ck/kernel.py
+++ ck/kernel.py
@@ -10,7 +10,7 @@
 # CK kernel - we made it monolithic with a minimal set 
 # of common functions for performance reasons
 
-__version__ = "1.6.15"
+__version__ = "1.6.15x"
 
 # Extra modules global for the whole kernel
 import sys
