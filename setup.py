fbf6ad1e8d4a7136d045330e3ff6c7e0b8609e40
Grigori Fursin <grigori.fursin@cTuning.org>
2016-05-21 17:31:37 +0200

    since we had a vulnerability fixed, we were suggested to release CK as a new subversion

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 
 setup(
   name='ck',
-  version='1.6.17dev',
+  version='1.7.1',
   url='https://github.com/ctuning/ck/wiki',
   license='BSD 3-clause',
   author='Grigori Fursin and non-profit cTuning foundation',
