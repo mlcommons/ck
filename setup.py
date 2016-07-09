131d811bd0d3057b28727fd6cb1feb1036c387c6
Grigori Fursin <grigori.fursin@cTuning.org>
2016-07-09 11:12:26 +0300

    Starting 1.7.3dev version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from distutils.util import convert_path
 
 setup(
   name='ck',
-  version='1.7.2',
+  version='1.7.3dev',
   url='https://github.com/ctuning/ck/wiki',
   license='BSD 3-clause',
   author='Grigori Fursin and non-profit cTuning foundation',
