932e4045a93cd2ca4777d8061471b438d03ad2ed
dsavenko <dsavenko@xored.com>
2016-09-08 13:34:21 +0700

    Run ck tests as an external process if unable to import python CK module

--- tests/test.py
+++ tests/test.py
@@ -9,11 +9,20 @@
 
 # Entry point for running CK tests for CI and coverage measurement tools
 
-import ck.kernel as ck
+from __future__ import print_function
 
-r = ck.access(['run', 'test'])
+try:
+    import ck.kernel as ck
 
-if 'return' not in r:
-  raise Exception('CK access function should always return key \'return\'!')
+    r = ck.access(['run', 'test'])
+    if 'return' not in r:
+        raise Exception('CK access function should always return key \'return\'!')
+    exit(int(r['return']))
 
-exit(int(r['return']))
+except ImportError as e:
+    from sys import stderr
+    from subprocess import call
+
+    print('WARNING: CK kernel module for python is not installed & jupyter notebooks will not be supported', file=stderr)
+    retcode = call(['ck', 'run', 'test'])
+    exit(retcode)
