97a71de2765d168ab91299c1a658740d64f406d6
dsavenko <dsavenko@xored.com>
2016-09-22 17:39:29 +0700

    Add docs to test_util functions

--- ck/repo/module/test/test_util.py
+++ ck/repo/module/test/test_util.py
@@ -12,19 +12,16 @@ from contextlib import contextmanager
 
 ck=None # Will be updated by CK (initialized CK kernel) 
 
-def dummy_exit(code):
-    print('Exit code: ' + str(code))
-
-def get_io(buf=''):
-    if sys.version_info[0]>2:
-       import io
-       return io.StringIO(buf)
-    else:
-       from StringIO import StringIO
-       return StringIO(buf)
+##############################################################################
+# Temporary files helper.
 
 @contextmanager
 def tmp_file(suffix='', prefix='ck-test-', content=''):
+    """
+    Yields a temporary file name. If content is given (not empty), 
+    creates the file with this content. Remove the file afterwards.
+    """
+
     fname = ck.gen_tmp_file({'suffix': suffix, 'prefix': prefix})['file_name']
     try:
         if content != '':
@@ -37,8 +34,20 @@ def tmp_file(suffix='', prefix='ck-test-', content=''):
         except OSError:
             pass
 
+##############################################################################
+# Temporary directory helper.
+
 @contextmanager
 def tmp_dir(suffix='', prefix='ck-test-', cwd=False):
+    """
+    Yields a temporary directory name (the directory is created). 
+    
+    If 'cwd' is True, make this directory the working directory. Restores the
+    original working directory afterwards.
+
+    Removes the directory and all its content afterwards.
+    """
+
     import shutil
     import tempfile
 
@@ -57,8 +66,22 @@ def tmp_dir(suffix='', prefix='ck-test-', cwd=False):
         except OSError:
             pass
 
+##############################################################################
+# System streams helper.
+
 @contextmanager
 def tmp_sys(input_buf=''):
+    """
+    Yields nothing, but substitutes standard input and output streams with 
+    StringIO streams. If 'input_buf' is given, make sure the new input stream
+    contains it.
+
+    Also substitutes the standard exit() routine with
+    the one which simple prints exit code. 
+
+    Restores the original streams and exit() afterwards.
+    """
+
     saved_stdout = sys.stdout
     saved_stdin = sys.stdin
     saved_exit = sys.exit
@@ -77,8 +100,16 @@ def tmp_sys(input_buf=''):
         sys.stdin = saved_stdin
         sys.exit = saved_exit
 
+##############################################################################
+# Temporary configuration helper.
+
 @contextmanager
 def tmp_cfg(cfg_key, cfg_value='yes'):
+    """
+    Yields nothing, but assignes the given configuration key (ck.cfg[cfg_key])
+    the given value. Restores the old value afterwards.
+    """
+
     saved_value = ck.cfg.get(cfg_key, None)
     try:
         ck.cfg[cfg_key] = cfg_value
@@ -89,8 +120,17 @@ def tmp_cfg(cfg_key, cfg_value='yes'):
         else:
            ck.cfg[cfg_key] = saved_value
 
+##############################################################################
+# Temporary repository helper.
+
 @contextmanager
 def tmp_repo(name='ck-test-repo'):
+    """
+    Creates a new repository and yields the information about it.
+
+    Removes the repository afterwards.
+    """
+
     r = ck.access({'module_uoa': 'repo', 'quiet': 'yes', 'data_uoa': name, 'action': 'add'})
     if 0 != r['return']:
        raise AssertionError('Failed to create a temporary repo ' + name)
@@ -101,3 +141,20 @@ def tmp_repo(name='ck-test-repo'):
         r = ck.access({'module_uoa': 'repo', 'quiet': 'yes', 'data_uoa': name, 'action': 'remove'})
         if 0 != r['return']:
            raise AssertionError('Failed to remove temporary repo ' + name + ' at location ' + path)
+
+##############################################################################
+# Prints exit code to the system output. Used internally
+
+def dummy_exit(code):
+    print('Exit code: ' + str(code))
+
+##############################################################################
+# Returns StringIO buffer. Used internnaly
+
+def get_io(buf=''):
+    if sys.version_info[0]>2:
+       import io
+       return io.StringIO(buf)
+    else:
+       from StringIO import StringIO
+       return StringIO(buf)
