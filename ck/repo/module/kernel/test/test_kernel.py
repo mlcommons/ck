1c9616b61abf2308dc963c34e1b98943339d9c7b
Dmitry Savenko <dsavenko@xored.com>
2016-09-25 17:15:39 +0800

    Improve tests failure reporting, fix Windows tests (#64)
    * Signal about failed tests with return code
    
    * Fix tests for Windows
    
    * Minor
    
    * Increase timeout for the locking test to make it more stable

--- ck/repo/module/kernel/test/test_kernel.py
+++ ck/repo/module/kernel/test/test_kernel.py
@@ -375,22 +375,24 @@ class TestKernel(unittest.TestCase):
             with open(fname + '/b/test.log', 'a') as f:
                 f.write('12')
 
+            s = os.sep
+
             r = ck.list_all_files({'path': fname, 'limit': 10})
             self.assertEqual(0, r['return'])
             t = r['list']
-            self.assertEqual({'size': 3}, t['a/test.txt'])
-            self.assertEqual({'size': 2}, t['b/test.log'])
+            self.assertEqual({'size': 3}, t['a' + s + 'test.txt'])
+            self.assertEqual({'size': 2}, t['b' + s + 'test.log'])
 
             r = ck.list_all_files({'path': fname, 'pattern': '*.txt'})
             self.assertEqual(0, r['return'])
             t = r['list']
-            self.assertEqual({'size': 3}, t['a/test.txt'])
-            self.assertEqual(None, t.get('b/test.log'))
+            self.assertEqual({'size': 3}, t['a' + s + 'test.txt'])
+            self.assertEqual(None, t.get('b' + s + 'test.log'))
 
             r = ck.list_all_files({'path': fname, 'file_name': 'test.txt'})
             self.assertEqual(0, r['return'])
             t = r['list']
-            self.assertEqual({'size': 3}, t['a/test.txt'])
+            self.assertEqual({'size': 3}, t['a' + s + 'test.txt'])
 
         with test_util.tmp_dir() as fname:
             with open(fname + '/test.txt', 'a') as f:
@@ -664,21 +666,30 @@ class TestKernel(unittest.TestCase):
         self.assertIn('path', r)
 
     def test_cd(self):
-        r = ck.cd({'module_uoa': 'kernel', 'data_uoa': 'default'})
-        self.assertEqual(0, r['return'])
-        self.assertEqual('cd ' + r['path'], r['string'])
-        # check for fields from ck.find:
-        self.assertEqual(1, r['number_of_entries'])
-        self.assertEqual('kernel', r['module_uoa'])
-        self.assertEqual('kernel', r['module_alias'])
-        self.assertEqual('default', r['data_uoa'])
-        self.assertEqual('local', r['repo_alias'])
-        self.assertEqual('local', r['repo_uoa'])
-        self.assertIn('repo_uid', r)
-        self.assertIn('module_uid', r)
-        self.assertIn('data_uid', r)
-        self.assertIn('path_repo', r)
-        self.assertIn('path', r)
+        r = ck.get_os_ck({})
+        plat = r['platform']
+
+        with test_util.tmp_sys():
+            r = ck.cd({'module_uoa': 'kernel', 'data_uoa': 'default'})
+            self.assertEqual(0, r['return'])
+
+            if plat == 'win':
+                self.assertEqual('cd /D ' + r['path'], r['string'])
+            else:
+                self.assertEqual('cd ' + r['path'], r['string'])
+
+            # check for fields from ck.find:
+            self.assertEqual(1, r['number_of_entries'])
+            self.assertEqual('kernel', r['module_uoa'])
+            self.assertEqual('kernel', r['module_alias'])
+            self.assertEqual('default', r['data_uoa'])
+            self.assertEqual('local', r['repo_alias'])
+            self.assertEqual('local', r['repo_uoa'])
+            self.assertIn('repo_uid', r)
+            self.assertIn('module_uid', r)
+            self.assertIn('data_uid', r)
+            self.assertIn('path_repo', r)
+            self.assertIn('path', r)
 
     def test_search(self):
         r = ck.search({'repo_uoa': 'default', 'module_uoa': 'kernel'})
@@ -745,9 +756,11 @@ class TestKernel(unittest.TestCase):
         self.assertEqual('no', r['found'])
 
     def test_list_files(self):
+        s = os.sep
+
         r = ck.list_files({'repo_uoa': 'default', 'module_uoa': 'module', 'data_uoa': 'kernel'})
         self.assertEqual(0, r['return'])
         lst = r['list']
         self.assertIn('module.py', lst)
-        self.assertIn('test/test_kernel.py', lst)
-        self.assertIn('test/test_original_tests.py', lst)
+        self.assertIn('test' + s + 'test_kernel.py', lst)
+        self.assertIn('test' + s + 'test_original_tests.py', lst)
