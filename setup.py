77237523db88095753a9ba1a9c7ee32f56df5366
Grigori Fursin <grigori.fursin@cTuning.org>
2016-11-15 15:47:35 +0100

    preparing for 1.8.4 release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from distutils.util import convert_path
 
 setup(
   name='ck',
-  version='1.8.3dev',
+  version='1.8.4',
   url='https://github.com/ctuning/ck/wiki',
   license='BSD 3-clause',
   author='Grigori Fursin and non-profit cTuning foundation',
