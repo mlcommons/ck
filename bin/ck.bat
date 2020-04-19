4d5cb8b91b7b425bc84b3e56fa2153eb4ee20e3e
Grigori Fursin <Grigori.Fursin@cTuning.org>
2020-04-19 16:26:15 +0200

    fixing scripts for windows

--- bin/ck.bat
+++ bin/ck.bat
@@ -2,13 +2,31 @@
 
 rem Set default path by detecting the path to this script
 set ck_path1=%~dp0
-set ck_path=%ck_path1%\..
+set ck_path2=%ck_path1%\..
 
-rem Check if CK_ROOT is defined and used it, otherwise use auto-detected path
-IF "%CK_ROOT%"=="" set CK_ROOT=%ck_path%
+if exist "%ck_path1%\ck-python.cfg%" (
+   set /p ck_python_pip=<"%ck_path1%\ck-python.cfg"
+)
+
+if exist "%ck_path1%\ck-root.cfg%" (
+   set /p ck_root_pip=<"%ck_path1%\ck-root.cfg"
+)
 
 IF "%CK_PYTHON%"=="" (
    set CK_PYTHON=python
+   if exist "%ck_python_pip%" (
+      set CK_PYTHON=%ck_python_pip%
+   )
+)
+
+IF "%CK_ROOT%"=="" (
+   if exist "%ck_root_pip%\ck\kernel.py" (
+      set CK_ROOT=%ck_root_pip%
+   )
+)
+
+IF "%CK_ROOT%"=="" (
+   set CK_ROOT=%ck_path2%
 )
 
 rem Load kernel module (either GIT/local installation or as package)
