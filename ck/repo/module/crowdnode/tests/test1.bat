da35de8f5f7117cb77c12526643c7bf858f6f791
Grigori Fursin <Grigori.Fursin@CodeReef.ai>
2020-11-17 10:29:58 +0100

    removed outdated crowdnode tests

--- ck/repo/module/crowdnode/tests/test1.bat
+++
@@ -1,1 +0,0 @@
-ck push crowdnode --filename=susan.exe --keyfile=D:\!FGG\Databases\Crypt\SSH\ck-crowdnode\key.txt --out=json --extra_path=tmp/xxx/yyy
--- ck/repo/module/crowdnode/tests/test2.bat
+++
@@ -1,1 +0,0 @@
-ck shell crowdnode --cmd="dir" --keyfile=D:\!FGG\Databases\Crypt\SSH\ck-crowdnode\key.txt
