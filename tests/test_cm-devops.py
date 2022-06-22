b87b83aae60ca3ab86f61f36641ff2f065c88626
Arjun <arjunsuresh1987@gmail.com>
2022-06-22 14:49:10 +0100

    Clean up of test_cm-devops

--- tests/test_cm-devops.py
+++ tests/test_cm-devops.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 try:
     import cmind as cm
 
@@ -7,16 +5,15 @@ try:
     if 'return' not in r:
         raise Exception('CM access function should always return key \'return\'!')
     
-    r = cm.access({'action':'search', 'automation':'cache', 'tags': 'python,src,install,_shared', 'version': '3.9.10'})
+    r = cm.access({'action':'search', 'automation':'cache', 'tags': 'python,src,install,_shared,version-3.9.10'})
     if 'return' not in r:
         raise Exception('CM access function should always return key \'return\'!')
-    if len(r['list']) <= 1:
+    if len(r['list']) < 1:
         raise Exception('CM search failed for the cached installation!')
     
     exit(0)
 
 except ImportError as e:
     from sys import stderr
-    from subprocess import call
-    retcode = call(['cm', 'run', 'script', '--tags=python,src,install,_shared --version=3.9.10'])
-    exit(retcode)
+    print('CM module for python is not installed', file=stderr)
+    exit(-1)
