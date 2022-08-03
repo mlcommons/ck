ff3093023d835a177b5b87b15e1c5993534804dc
Arjun <arjunsuresh1987@gmail.com>
2022-08-04 00:01:34 +0100

    Added get-sut-mlc-configs

---
+++ cm-mlops/script/get-sut-mlc-configs/customize.py
@@ -0,0 +1,25 @@
+from cmind import utils
+import os
+import yaml
+
+def preprocess(i):
+    env = i['env']
+    state = i['state']
+    if 'CM_SUT_NAME' not in env:
+        env['CM_SUT_NAME'] = "dummy"
+    sut = env['CM_SUT_NAME']
+    path = i['run_script_input']['path']
+    config_path = os.path.join(path, "configs", env['CM_SUT_NAME'], "config.yaml")
+    if not os.path.exists(config_path):
+        print("SUT config file not present")
+        return {'return':-1}
+    if 'CM_SUT_CONFIG' not in state:
+        state['CM_SUT_CONFIG'] = {}
+    state['CM_SUT_CONFIG'][env['CM_SUT_NAME']] = yaml.load(open(config_path))
+    return {'return':0}
+
+def postprocess(i):
+
+    env = i['env']
+
+    return {'return':0}
