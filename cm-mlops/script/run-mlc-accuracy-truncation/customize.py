a39a9678b87d09b0ab367eee3525450c418cddc2
Arjun <arjunsuresh1987@gmail.com>
2022-08-03 14:30:41 +0100

    Added run-mlc-accuracy-truncation

---
+++ cm-mlops/script/run-mlc-accuracy-truncation/customize.py
@@ -0,0 +1,21 @@
+from cmind import utils
+import cmind as cm
+import os
+import subprocess
+from os.path import exists
+
+def preprocess(i):
+
+    os_info = i['os_info']
+    env = i['env']
+    submission_dir = env.get("CM_MLC_SUBMISSION_DIR", "")
+    if submission_dir == "":
+        print("Please set CM_MLC_SUBMISSION_DIR")
+        exit(-1)
+    submitter = env.get("CM_MLC_SUBMITTER", "dummy")
+    os.system("rm -rf " + submission_dir + "_logs")
+    CMD = env['CM_PYTHON_BIN'] + ' ' + os.path.join(env['CM_MLC_INFERENCE_SOURCE'], "tools", "submission",
+            "truncate_accuracy_log.py") + " --input " + submission_dir + " --submitter " + submitter + " --backup " + submission_dir + "_logs"
+    os.system(CMD)
+
+    return {'return':0}
