095d239ccf74ba38b72ccd0e2a4025d347d70836
Arjun Suresh <arjunsuresh1987@gmail.com>
2022-09-06 14:39:44 +0100

    Update macos cpuinfo

--- cm-mlops/script/detect-cpu/run.sh
+++ cm-mlops/script/detect-cpu/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ ${CM_HOST_OS_FLAVOR} == "macos" ]]; then
-    sysctl -a | grep machdep.cpu > tmp-lscpu.out
+    sysctl -a | grep hw > tmp-lscpu.out
 else
     lscpu > tmp-lscpu.out
 fi
