4c0babf7685cf7baa99c78f711a191790f60ddcd
Arjun <arjunsuresh1987@gmail.com>
2022-09-07 11:40:49 +0100

    Script install test added for #275

--- tests/script/test_install.py
+++ tests/script/test_install.py
@@ -4,6 +4,8 @@ try:
     r = cm.access({'action':'run', 'automation':'script', 'tags': 'python,src,install,_shared', 'version': '3.9.10', 'quiet': 'true'})
     if 'return' not in r:
         raise Exception('CM access function should always return key \'return\'!')
+    if 'error' in r:
+        raise Exception(r['error'])
     
     r = cm.access({'action':'search', 'automation':'cache', 'tags': 'python,src,install,_shared,version-3.9.10'})
     if 'return' not in r:
