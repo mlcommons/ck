6d7bd31a5e34bdd4a4d2d00196defab8637d5437
Arjun <arjunsuresh1987@gmail.com>
2022-09-07 16:20:46 +0100

    HOST_TOTAL_CORES -> HOST_CPU_TOTAL_CORES

--- cm-mlops/script/install-gcc-src/run.sh
+++ cm-mlops/script/install-gcc-src/run.sh
@@ -25,7 +25,7 @@ cd ../build
 if [ "${?}" != "0" ]; then exit 1; fi
 
 echo "******************************************************"
-CM_MAKE_CORES=${CM_MAKE_CORES:-${CM_HOST_TOTAL_CORES}}
+CM_MAKE_CORES=${CM_MAKE_CORES:-${CM_HOST_CPU_TOTAL_CORES}}
 CM_MAKE_CORES=${CM_MAKE_CORES:-2}
 
 make -j${CM_MAKE_CORES}
--- cm-mlops/script/install-mlc-inference-loadgen/run.sh
+++ cm-mlops/script/install-mlc-inference-loadgen/run.sh
@@ -17,7 +17,7 @@ cmake \
 if [ "${?}" != "0" ]; then exit 1; fi
 
 echo "******************************************************"
-CM_MAKE_CORES=${CM_MAKE_CORES:-${CM_HOST_TOTAL_CORES}}
+CM_MAKE_CORES=${CM_MAKE_CORES:-${CM_HOST_CPU_TOTAL_CORES}}
 CM_MAKE_CORES=${CM_MAKE_CORES:-2}
 
 cmake --build . --target install -j${CM_MAKE_CORES}
