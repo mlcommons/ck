fec86d5ab056cde24152e6e4b64f012765e86cf1
Arjun <arjunsuresh1987@gmail.com>
2022-09-07 17:34:25 +0100

    Added quiet option to test script

--- tests/script/test_deps.py
+++ tests/script/test_deps.py
@@ -4,7 +4,7 @@ try:
 
     r = cm.access({'action':'run', 'automation':'script', 'tags': 'generate-run-cmds,mlperf', 'add_deps_recursive':
         {'inference-src': {'tags': '_octoml'}, 'loadgen': {'version': 'r2.1'}}, 'env': {'CM_MODEL': 'resnet50',
-            'CM_DEVICE': 'cpu', 'CM_BACKEND': 'onnxruntime'}})
+            'CM_DEVICE': 'cpu', 'CM_BACKEND': 'onnxruntime'}, 'quiet': 'yes'})
     if 'return' not in r:
         raise Exception('CM access function should always return key \'return\'!')
     if 'error' in r:
