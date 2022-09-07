9f48b14184894869cd12aa04cc60ed86400f6113
Arjun <arjunsuresh1987@gmail.com>
2022-09-07 17:06:34 +0100

    Added test_deps for cm-scripts, #275

---
+++ tests/script/test_deps.py
@@ -0,0 +1,18 @@
+# This test covers version, variation, compilation from src, add_deps_recursive, post_deps
+try:
+    import cmind as cm
+
+    r = cm.access({'action':'run', 'automation':'script', 'tags': 'generate-run-cmds,mlperf', 'add_deps_recursive':
+        {'inference-src': {'tags': '_octoml'}, 'loadgen': {'version': 'r2.1'}}, 'env': {'CM_MODEL': 'resnet50',
+            'CM_DEVICE': 'cpu', 'CM_BACKEND': 'onnxruntime'}})
+    if 'return' not in r:
+        raise Exception('CM access function should always return key \'return\'!')
+    if 'error' in r:
+        raise Exception(r['error'])
+    
+    exit(0)
+
+except ImportError as e:
+    from sys import stderr
+    print('CM module for python is not installed', file=stderr)
+    exit(1)
