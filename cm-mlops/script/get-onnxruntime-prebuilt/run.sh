b522d6de4b886edc0ed709821d27c7c14ac2b91b
Arjun <arjunsuresh1987@gmail.com>
2022-09-16 22:56:18 +0100

    get-onnxruntime-prebuilt script added

---
+++ cm-mlops/script/get-onnxruntime-prebuilt/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CM_VERSION=${CM_VERSION:-1.12.1}
+
+mkdir -p install
+machine=${CM_HOST_OS_MACHINE:-x86_64}
+if [[ $machine == "x86_64" ]]; then
+  machine=x64
+fi
+FOLDER=onnxruntime-${CM_HOST_OS_TYPE}-$machine-${CM_VERSION}
+FILENAME=$FOLDER".tgz"
+WGET_CMD="-q --no-check-certificate https://github.com/microsoft/onnxruntime/releases/download/v${CM_VERSION}/${FILENAME}"
+echo "wget ${WGET_CMD}"
+wget ${WGET_CMD}
+test $? -eq 0 || exit 1
+tar -C install -xzf ${FILENAME}
+test $? -eq 0 || exit 1
+echo "CM_TMP_INSTALL_FOLDER=$FOLDER" > tmp-run-env.out
+
