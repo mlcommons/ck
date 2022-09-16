e3818eb9c7c6b676858343dda99b33ef30fd13cd
Arjun <arjunsuresh1987@gmail.com>
2022-09-16 18:19:19 +0100

    Added pip dependency for imagenet-preprocess

--- cm-mlops/script/get-imagenet-preprocessed/run.sh
+++ cm-mlops/script/get-imagenet-preprocessed/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-${CM_PYTHON_BIN} -m pip install ${CM_TMP_CURRENT_SCRIPT_PATH}/requirements.txt
+${CM_PYTHON_BIN} -m pip install -r ${CM_TMP_CURRENT_SCRIPT_PATH}/requirements.txt
 ${CM_PYTHON_BIN} ${CM_TMP_CURRENT_SCRIPT_PATH}/preprocess.py
