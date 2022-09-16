96fc84d897b072c3c1ac13605004817baaee76c2
Arjun <arjunsuresh1987@gmail.com>
2022-09-16 17:33:40 +0100

    Imagenet preprocess script added for mlperf

--- cm-mlops/script/get-imagenet-preprocessed/run.sh
+++ cm-mlops/script/get-imagenet-preprocessed/run.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-CM_TMP_CURRENT_SCRIPT_PATH=${CM_TMP_CURRENT_SCRIPT_PATH:-$PWD}
-${CM_PYTHON_BIN} -m pip install -r ${CM_TMP_CURRENT_SCRIPT_PATH}/requirements.txt
-test $? -eq 0 || exit 1
-
-wget -nc https://raw.githubusercontent.com/mlcommons/ck-mlops/main/program/ml-task-image-classification-tvm-onnx-cpu/synset.txt
-test $? -eq 0 || exit 1
-
-mkdir preprocessed
-
-${CM_PYTHON_BIN} ${CM_TMP_CURRENT_SCRIPT_PATH}/src/classify.py --image ${CM_DATASET_PATH}/ILSVRC2012_val_00000001.JPEG 
-test $? -eq 0 || exit 1
-
-cd preprocessed && ls *.raw > names.txt && cd ../
-test $? -eq 0 || exit 1
-
-echo "CM_DATASET_PREPROCESSED_PATH=$PWD/preprocessed" > tmp-run-env.out
+${CM_PYTHON_BIN} ${CM_TMP_CURRENT_SCRIPT_PATH}/preprocess.py
