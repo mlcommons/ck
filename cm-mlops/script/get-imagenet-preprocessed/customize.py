fd79c39368c8524985fe1d061e2f013dc7814f3a
Arjun <arjunsuresh1987@gmail.com>
2022-09-16 18:35:15 +0100

    Fixes

--- cm-mlops/script/get-imagenet-preprocessed/customize.py
+++ cm-mlops/script/get-imagenet-preprocessed/customize.py
@@ -1,11 +1,15 @@
 from cmind import utils
 import os
+import shutil
 
 def preprocess(i):
 
     env = i['env']
     if 'CM_DATASET_PREPROCESSED_PATH' not in env:
         env['CM_DATASET_PREPROCESSED_PATH'] = os.getcwd()
+
+    shutil.copy(os.path.join(env['CM_DATASET_AUX_PATH'], "val.txt"), os.path.join(env['CM_DATASET_PATH'],
+        "val_map.txt"))
 
     print("Using MLCommons Inference source from '" + env['CM_MLC_INFERENCE_SOURCE'] +"'")
 
