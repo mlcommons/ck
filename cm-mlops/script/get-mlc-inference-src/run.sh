cf304f352a5fec53412250d0dfbde7c523640a95
Arjun <arjunsuresh1987@gmail.com>
2022-09-16 15:20:55 +0100

    Added pybind11 update to inference-src

--- cm-mlops/script/get-mlc-inference-src/run.sh
+++ cm-mlops/script/get-mlc-inference-src/run.sh
@@ -9,6 +9,9 @@ echo "Cloning Mlcommons from ${CM_GIT_URL} with branch ${CM_GIT_CHECKOUT} ${CM_G
 if [ ! -d "inference" ]; then
   git clone ${CM_GIT_RECURSE_SUBMODULES} -b "${CM_GIT_CHECKOUT}" ${CM_GIT_URL} ${CM_GIT_DEPTH} inference
   if [ "${?}" != "0" ]; then exit 1; fi
+  if [ -z ${CM_GIT_RECURSE_SUBMODULES} ]; then #needed to build loadgen
+    git submodule update third_party/pybind
+  fi
 fi
 
 if [ ${CM_GIT_PATCH} == "yes" ]; then
