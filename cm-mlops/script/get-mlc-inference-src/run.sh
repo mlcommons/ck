3d564eb602eaece2785680753a11dc78cd16dcaf
Arjun <arjunsuresh1987@gmail.com>
2022-09-16 13:53:42 +0100

    Improved inference src script, fix for #314

--- cm-mlops/script/get-mlc-inference-src/run.sh
+++ cm-mlops/script/get-mlc-inference-src/run.sh
@@ -4,10 +4,10 @@ CUR_DIR=$PWD
 SCRIPT_DIR=${CM_TMP_CURRENT_SCRIPT_PATH}
 
 echo "******************************************************"
-echo "Cloning Mlcommons from ${CM_GIT_URL} with branch ${CM_GIT_CHECKOUT}..."
+echo "Cloning Mlcommons from ${CM_GIT_URL} with branch ${CM_GIT_CHECKOUT} ${CM_GIT_DEPTH} ${CM_GIT_RECURSE_SUBMODULES}..."
 
 if [ ! -d "inference" ]; then
-  git clone --recurse-submodules -b "${CM_GIT_CHECKOUT}" ${CM_GIT_URL} inference
+  git clone ${CM_GIT_RECURSE_SUBMODULES} -b "${CM_GIT_CHECKOUT}" ${CM_GIT_URL} ${CM_GIT_DEPTH} inference
   if [ "${?}" != "0" ]; then exit 1; fi
 fi
 
