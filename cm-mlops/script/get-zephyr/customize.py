7223d46dc2d53f92c66726db1a63175e55a978db
Arjun Suresh <arjunsuresh1987@gmail.com>
2022-09-21 14:43:51 +0100

    Added path for west install

--- cm-mlops/script/get-zephyr/customize.py
+++ cm-mlops/script/get-zephyr/customize.py
@@ -2,7 +2,10 @@ from cmind import utils
 import os
 
 def preprocess(i):
-
+    env = i['env']
+    if '+PATH' not in env:
+        env['+PATH'] = []
+    env['+PATH'].append("$HOME/.local/bin")
     return {'return':0}
 
 
