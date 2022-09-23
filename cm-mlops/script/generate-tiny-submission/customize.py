ae0f4c20c13bfe3214a0842b14e3f65ef9240be7
Arjun Suresh <arjunsuresh1987@gmail.com>
2022-09-23 09:42:18 +0100

    generate-tiny-submission script cleanup

--- cm-mlops/script/generate-tiny-submission/customize.py
+++ cm-mlops/script/generate-tiny-submission/customize.py
@@ -6,20 +6,22 @@ def preprocess(i):
 
     os_info = i['os_info']
     env = i['env']
-    script_tags =  "reproduce,tiny,app"
+    state = i['state']
+    inp = i['input']
+    script_tags =  "reproduce,tiny"
     microtvm_variants = { "cmsis_nn": [ "ad", "ic", "vww", "kws" ], "native": [ "ic"] }
     for microtvm_variant in microtvm_variants:
-        for model in microtvm_variants[microtvm_variant]
-        variation_tags_string="_"+microtvm_variant+"_"+model
-        tags = script_tags + variation_tags_string
-        r = cm.access({'action':'run', 'automation':'script', 'tags': tags, 'quiet': 'true', 'env': env, 
-            'input': inp, 'state': state, 'add_deps': inp.get('add_deps', {}), 'add_deps_recursive':
+        for model in microtvm_variants[microtvm_variant]:
+            variation_tags_string="_"+microtvm_variant+",_"+model
+            tags = script_tags + "," + variation_tags_string
+            r = cm.access({'action':'run', 'automation':'script', 'tags': tags, 'quiet': 'true', 'env': env, 
+                'input': inp, 'state': state, 'add_deps': inp.get('add_deps', {}), 'add_deps_recursive':
                     inp.get('add_deps_recursive', {})})
-        if r['return'] > 0:
-            return r
+            if r['return'] > 0:
+                return r
 
     return {'return':0}
 
 def postprocess(i):
-    print("The binaries to flash can be found by \"find `cm find cache --tags=microtvm,source` --name zephyr.elf\"") 
+    print("The binaries to flash can be found by the below command \n\t find `cm find cache --tags=microtvm,source` -name zephyr.elf") 
     return {'return':0}
