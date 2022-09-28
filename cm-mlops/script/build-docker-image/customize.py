2b38da17874bd835fffa192ae95692b5151eeecb
Arjun <arjunsuresh1987@gmail.com>
2022-09-28 15:13:14 +0100

    Added Docker ARGS

--- cm-mlops/script/build-docker-image/customize.py
+++ cm-mlops/script/build-docker-image/customize.py
@@ -6,6 +6,14 @@ def preprocess(i):
 
     os_info = i['os_info']
     env = i['env']
+    CM_DOCKER_BUILD_ARGS = []
+    if 'CM_GH_TOKEN' in env:
+        CM_DOCKER_BUILD_ARGS.append( "GH_TOKEN="+env['CM_GH_TOKEN'] )
+    if CM_DOCKER_BUILD_ARGS:
+        build_args = "--build-arg "+ "--build-arg".join(CM_DOCKER_BUILD_ARGS)
+    else:
+        build_args = ""
+    env['CM_DOCKER_BUILD_ARGS'] = build_args
     if 'CM_DOCKERFILE_WITH_PATH' not in env or not exists(env['CM_DOCKERFILE_WITH_PATH']):
         env['CM_BUILD_DOCKERFILE'] = "yes"
         env['CM_RUN_DOCKERFILE'] = "yes"
