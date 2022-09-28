fce5191caaecf1c0105891d147bb8b1489fb7b7a
Arjun <arjunsuresh1987@gmail.com>
2022-09-28 08:36:12 +0100

    Generalize dockerfile for rhel and built base dockerfiles for CM #212, #351

--- cm-mlops/script/build-dockerfile/customize.py
+++ cm-mlops/script/build-dockerfile/customize.py
@@ -1,61 +1,92 @@
 from cmind import utils
 import os
+import json
 
 def preprocess(i):
 
     os_info = i['os_info']
     env = i['env']
-    if "CM_DOCKER_IMAGE_BASE" not in env:
-        env["CM_DOCKER_IMAGE_BASE"] = "ubuntu:20.04"
+    if 'CM_DOCKER_IMAGE_EOL' not in env:
+        env['CM_DOCKER_IMAGE_EOL'] = "\n"
+    if "CM_DOCKER_OS" not in env:
+        env["CM_DOCKER_OS"] = "ubuntu"
+    if env["CM_DOCKER_OS"] not in [ "ubuntu", "rhel" ]:
+        return {'return': 1, 'error': "Currently only ubuntu and rhel are supported in CM docker"}
+    with open("dockerinfo.json") as f:
+        config = json.load(f)
+
+    if "CM_DOCKER_OS_VERSION" not in env:
+        env["CM_DOCKER_OS_VERSION"] = "20.04"
+
+    docker_image_base = get_value(env, config, 'FROM', 'CM_DOCKER_IMAGE_BASE')
+    if not docker_image_base:
+        if env["CM_DOCKER_OS"] == "ubuntu":
+            docker_image_base = env["CM_DOCKER_OS"]+":"+env["CM_DOCKER_OS_VERSION"]
+        else:
+            return {'return': 1, 'error': "Version "+env["CM_DOCKER_OS_VERSION"]+" is not supported yet"}
+
     if "CM_MLOPS_REPO" in env:
         cm_mlops_repo = env["CM_MLOPS_REPO"]
     else:
         cm_mlops_repo = "mlcommons@ck"
+
     if 'CM_DOCKERFILE_WITH_PATH' not in env:
         env['CM_DOCKERFILE_WITH_PATH'] = os.path.join(os.getcwd(), "Dockerfile")
     if 'CM_DOCKER_IMAGE_RUN_CMD' not in env:
         env['CM_DOCKER_IMAGE_RUN_CMD'] = "cm version"
     f = open(env['CM_DOCKERFILE_WITH_PATH'], "w")
     EOL = env['CM_DOCKER_IMAGE_EOL']
-    f.write('FROM ' + env['CM_DOCKER_IMAGE_BASE'] + EOL)
-    if 'CM_DOCKER_IMAGE_LABEL' in env and env['CM_DOCKER_IMAGE_LABEL']:
-        f.write('LABEL ' + env['CM_DOCKER_IMAGE_LABEL'] + EOL)
-    if 'CM_DOCKER_IMAGE_SHELL' in env and env['CM_DOCKER_IMAGE_SHELL']:
-        f.write('SHELL ' + env['CM_DOCKER_IMAGE_SHELL'] + EOL)
-    f.write('RUN apt update -y' + EOL)
-    f.write('RUN apt install -y python3 python3-pip git sudo' + EOL)
-    f.write('RUN python3 -m pip install cmind requests' + EOL)
-    if 'CM_DOCKER_IMAGE_ENTRYPOINT' in env and env['CM_DOCKER_IMAGE_ENTRYPOINT']:
-        f.write('ENTRYPOINT ' + env['CM_DOCKER_IMAGE_ENTRYPOINT'] + EOL)
-    if 'CM_DOCKER_IMAGE_TZ' in env:
-        f.write('ENV TZ=' + env['CM_DOCKER_IMAGE_TZ'] + EOL)
-        f.write('RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ >/etc/timezone' + EOL)
-
-    if 'CM_DOCKER_USER' in env:
-        if 'CM_DOCKER_GROUP' not in env:
-            env['CM_DOCKER_GROUP'] = env['CM_DOCKER_USER']
-        DOCKER_GROUP = ' -g ' + env['CM_DOCKER_GROUP']
-        if 'CM_DOCKER_GROUP_ID' in env:
-            DOCKER_GROUP_ID = "-g " + env['CM_DOCKER_GROUP_ID']
+    f.write('FROM ' + docker_image_base + EOL)
+    image_label = get_value(env, config, 'LABEL', 'CM_DOCKER_IMAGE_LABEL')
+    if image_label:
+        f.write('LABEL ' + image_label + EOL)
+    shell = get_value(env, config, 'SHELL', 'CM_DOCKER_IMAGE_SHELL')
+    if shell:
+        f.write('SHELL ' + shell + EOL)
+    f.write('RUN ' + get_value(env, config, 'package-manager-update-cmd') + EOL)
+    f.write('RUN '+ get_value(env, config, 'package-manager-get-cmd') + " " + " ".join(get_value(env, config,
+        'packages')) + EOL)
+    f.write('RUN python3 -m pip install ' + " ".join(get_value(env, config, 'python-packages')) + EOL)
+    entry_point = get_value(env, config, 'ENTRYPOINT', 'CM_DOCKER_IMAGE_ENTRYPOINT')
+    if entry_point:
+        f.write('ENTRYPOINT ' + entry_point + EOL)
+    for key,value in config['ENV'].items():
+        f.write('ENV '+ key + "=" + value + EOL)
+    for cmd in config['RUN_CMDS']:
+        f.write('RUN '+ cmd + EOL)
+    docker_user = get_value(env, config, 'USER', 'CM_DOCKER_USER')
+    docker_userid = get_value(env, config, 'USERID', 'CM_DOCKER_USER_ID')
+    docker_group = get_value(env, config, 'GROUP', 'CM_DOCKER_GROUP')
+    docker_groupid = get_value(env, config, 'GROUPID', 'CM_DOCKER_GROUP_ID')
+    if docker_user:
+        if not docker_group:
+            docker_group = docker_user
+        DOCKER_GROUP = ' -g ' + docker_group
+        if docker_groupid:
+            DOCKER_GROUP_ID = "-g " + docker_groupid
         else:
             DOCKER_GROUP_ID = ""
-        f.write('RUN groupadd ' + DOCKER_GROUP_ID + env['CM_DOCKER_GROUP'] + EOL)
-        if 'CM_DOCKER_USER_ID' in env:
-            DOCKER_USER_ID = "-u " + env['CM_DOCKER_USER_ID']
+        f.write('RUN groupadd ' + DOCKER_GROUP_ID + docker_group + EOL)
+        if docker_userid:
+            DOCKER_USER_ID = "-u " + docker_userid
         else:
             DOCKER_USER_ID = ""
-        f.write('RUN useradd ' + DOCKER_USER_ID  + DOCKER_GROUP + ' --create-home --shell /bin/bash '
-                + env['CM_DOCKER_USER'] + EOL)
-        f.write('RUN echo "' + env['CM_DOCKER_USER'] + ' ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers' + EOL)
-        f.write('USER ' + env['CM_DOCKER_USER'] + ":" + env['CM_DOCKER_GROUP'] + EOL)
-    if 'CM_DOCKER_WORKDIR' in env:
-        f.write('WORKDIR ' + env['CM_DOCKER_WORKDIR'] + EOL)
+        f.write('RUN useradd ' + DOCKER_USER_ID  + DOCKER_GROUP + ' --create-home --shell '+ config['SHELL'] + ' ' 
+                + docker_user + EOL)
+        f.write('RUN echo "' + docker_user + ' ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers' + EOL)
+        f.write('USER ' + docker_user + ":" + docker_group + EOL)
+    workdir = get_value(env, config, 'WORKDIR', 'CM_DOCKER_WORKDIR')
+    if workdir:
+        f.write('WORKDIR ' + workdir + EOL)
     f.write('RUN cm pull repo ' + cm_mlops_repo + EOL)
     #echo '${CM_DOCKER_IMAGE_ENV}'
 
     f.write('RUN cm run script --quiet --tags=get,sys-utils-cm' + EOL)
-
-    f.write('RUN ' + env['CM_DOCKER_IMAGE_RUN_CMD'] + " --fake_run" + EOL)
+    if "run" in env['CM_DOCKER_IMAGE_RUN_CMD']:
+        fake_run = " --fake_run"
+    else:
+        fake_run = ""
+    f.write('RUN ' + env['CM_DOCKER_IMAGE_RUN_CMD'] + fake_run + EOL)
 
     f.close()
 
@@ -63,3 +94,20 @@ def preprocess(i):
     print(f.read())
 
     return {'return':0}
+
+def get_value(env, config, key, env_key = None):
+    if not env_key:
+        env_key = key
+    if env_key in env:
+        return env[env_key]
+    docker_os = env['CM_DOCKER_OS']
+    docker_os_version = env['CM_DOCKER_OS_VERSION']
+    version_meta = config['distros'][docker_os]['versions'].get(docker_os_version, {})
+    if key in version_meta:
+        return version_meta[key]
+    distro_meta = config['distros'][docker_os]
+    if key in distro_meta:
+        return distro_meta[key]
+    if key in config:
+        return config[key]
+    return None
