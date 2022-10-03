6340e67b4ef1c157c19d474b12c73dc0b82bc5bf
Arjun Suresh <arjunsuresh1987@gmail.com>
2022-10-03 10:10:17 +0100

    Calling install-script as prehook_deps for llvm

--- cm-mlops/script/get-llvm/customize.py
+++ cm-mlops/script/get-llvm/customize.py
@@ -11,9 +11,11 @@ def preprocess(i):
 
     file_name_c = 'clang.exe' if os_info['platform'] == 'windows' else 'clang'
     file_name_cpp = 'clang++.exe' if os_info['platform'] == 'windows' else 'clang++'
+    env['FILE_NAME_C'] = file_name_c
+    env['FILE_NAME_CPP'] = file_name_cpp
+    if 'CM_LLVM_INSTALLED_PATH' not in env:
 
-    # Will check env['CM_TMP_PATH'] if comes from installation script
-    r = i['automation'].find_artifact({'file_name': file_name_c,
+        r = i['automation'].find_artifact({'file_name': file_name_c,
                                        'env': env,
                                        'os_info':os_info,
                                        'default_path_env_key': 'PATH',
@@ -21,20 +23,39 @@ def preprocess(i):
                                        'env_path_key':'CM_LLVM_CLANG_BIN_WITH_PATH',
                                        'run_script_input':i['run_script_input'],
                                        'recursion_spaces':recursion_spaces})
-    if r['return'] >0 : 
-       if r['return'] == 16:
-           if env.get('CM_TMP_FAIL_IF_NOT_FOUND','').lower() == 'yes':
-               return r
+        if r['return'] >0 : 
+            if r['return'] == 16:
+                env['CM_TMP_REQUIRE_INSTALL'] = "yes"
+                return {'return': 0}
+            else:
+                return r
 
-           print (recursion_spaces+'    # {}'.format(r['error']))
+        env['CM_LLVM_INSTALLED_PATH'] =  r['found_path']
 
-           # Attempt to run installer
-           r = {'return':0, 'skip':True, 'script':{'tags':'install,prebuilt,llvm'}}
+    return {'return':0}
 
-       return r
 
-    found_path = r['found_path']
+def postprocess(i):
 
+    env = i['env']
+    r = i['automation'].parse_version({'match_text': r'clang version\s*([\d.]+)',
+                                       'group_number': 1,
+                                       'env_key':'CM_LLVM_CLANG_VERSION',
+                                       'which_env':i['env']})
+    if r['return'] >0: return r
+
+    version = r['version']
+
+    print (i['recursion_spaces'] + '    Detected version: {}'.format(version))
+
+    env['CM_LLVM_CLANG_CACHE_TAGS'] = 'version-'+version
+    env['CM_COMPILER_CACHE_TAGS'] = 'version-'+version+',family-llvm'
+    env['CM_COMPILER_FAMILY'] = 'LLVM'
+    env['CM_COMPILER_VERSION'] = env['CM_LLVM_CLANG_VERSION']
+
+    file_name_c = env['FILE_NAME_C']
+    file_name_cpp = env['FILE_NAME_CPP']
+    found_path = env['CM_LLVM_INSTALLED_PATH']
     env['CM_LLVM_CLANG_BIN']=file_name_c
     env['CM_LLVM_CLANG_BIN_WITH_PATH']=os.path.join(found_path, file_name_c)
 
@@ -51,27 +72,5 @@ def preprocess(i):
     env['DEBUG_LINKER_FLAGS'] = "-O0"
     env['DEFAULT_COMPILER_FLAGS'] = "-O2"
     env['DEFAULT_LINKER_FLAGS'] = "-O2"
-
-    return {'return':0}
-
-
-def postprocess(i):
-
-    env = i['env']
-
-    r = i['automation'].parse_version({'match_text': r'clang version\s*([\d.]+)',
-                                       'group_number': 1,
-                                       'env_key':'CM_LLVM_CLANG_VERSION',
-                                       'which_env':i['env']})
-    if r['return'] >0: return r
-
-    version = r['version']
-
-    print (i['recursion_spaces'] + '    Detected version: {}'.format(version))
-
-    env['CM_LLVM_CLANG_CACHE_TAGS'] = 'version-'+version
-    env['CM_COMPILER_CACHE_TAGS'] = 'version-'+version+',family-llvm'
-    env['CM_COMPILER_FAMILY'] = 'LLVM'
-    env['CM_COMPILER_VERSION'] = env['CM_LLVM_CLANG_VERSION']
 
     return {'return':0, 'version':version}
--- cm-mlops/script/get-llvm/run.sh
+++ cm-mlops/script/get-llvm/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-${CM_LLVM_CLANG_BIN_WITH_PATH} --version > tmp-ver.out
+clang_bin=${CM_LLVM_INSTALLED_PATH}/${FILE_NAME_C}
+${clang_bin} --version > tmp-ver.out
 test $? -eq 0 || exit 1
