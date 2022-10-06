e4af7efd3a687d4c3009163477b1023c122a01f8
Arjun Suresh <arjunsuresh1987@gmail.com>
2022-10-06 11:41:41 +0100

    new_env_keys added for ml-models

--- cm-mlops/script/get-imagenet-helper/imagenet_helper/__init__.py
+++ cm-mlops/script/get-imagenet-helper/imagenet_helper/__init__.py
@@ -6,26 +6,26 @@ import numpy as np
 
 ## Processing in batches:
 #
-BATCH_SIZE              = int(os.getenv('CK_BATCH_SIZE', 1))
+BATCH_SIZE              = int(os.getenv('CM_BATCH_SIZE', 1))
 
 
 ## Model properties:
 #
-MODEL_IMAGE_HEIGHT      = int(os.getenv('ML_MODEL_IMAGE_HEIGHT',
-                              os.getenv('CK_ENV_ONNX_MODEL_IMAGE_HEIGHT',
-                              os.getenv('CK_ENV_TENSORFLOW_MODEL_IMAGE_HEIGHT',
+MODEL_IMAGE_HEIGHT      = int(os.getenv('CM_ML_MODEL_IMAGE_HEIGHT',
+                              os.getenv('CM_ONNX_MODEL_IMAGE_HEIGHT',
+                              os.getenv('CM_TENSORFLOW_MODEL_IMAGE_HEIGHT',
                               ''))))
-MODEL_IMAGE_WIDTH       = int(os.getenv('ML_MODEL_IMAGE_WIDTH',
-                              os.getenv('CK_ENV_ONNX_MODEL_IMAGE_WIDTH',
-                              os.getenv('CK_ENV_TENSORFLOW_MODEL_IMAGE_WIDTH',
+MODEL_IMAGE_WIDTH       = int(os.getenv('CM_ML_MODEL_IMAGE_WIDTH',
+                              os.getenv('CM_ONNX_MODEL_IMAGE_WIDTH',
+                              os.getenv('CM_TENSORFLOW_MODEL_IMAGE_WIDTH',
                               ''))))
-MODEL_IMAGE_CHANNELS    = int(os.getenv('ML_MODEL_IMAGE_CHANNELS', 3))
-MODEL_DATA_LAYOUT       = os.getenv('ML_MODEL_DATA_LAYOUT', 'NCHW')
-MODEL_COLOURS_BGR       = os.getenv('ML_MODEL_COLOUR_CHANNELS_BGR', 'NO') in ('YES', 'yes', 'ON', 'on', '1')
-MODEL_INPUT_DATA_TYPE   = os.getenv('ML_MODEL_INPUT_DATA_TYPE', 'float32')
-MODEL_DATA_TYPE         = os.getenv('ML_MODEL_DATA_TYPE', '(unknown)')
-MODEL_USE_DLA           = os.getenv('ML_MODEL_USE_DLA', 'NO') in ('YES', 'yes', 'ON', 'on', '1')
-MODEL_MAX_BATCH_SIZE    = int(os.getenv('ML_MODEL_MAX_BATCH_SIZE', BATCH_SIZE))
+MODEL_IMAGE_CHANNELS    = int(os.getenv('CM_ML_MODEL_IMAGE_CHANNELS', 3))
+MODEL_DATA_LAYOUT       = os.getenv('CM_ML_MODEL_DATA_LAYOUT', 'NCHW')
+MODEL_COLOURS_BGR       = os.getenv('CM_ML_MODEL_COLOUR_CHANNELS_BGR', 'NO') in ('YES', 'yes', 'ON', 'on', '1')
+MODEL_INPUT_DATA_TYPE   = os.getenv('CM_ML_MODEL_INPUT_DATA_TYPE', 'float32')
+MODEL_DATA_TYPE         = os.getenv('CM_ML_MODEL_DATA_TYPE', '(unknown)')
+MODEL_USE_DLA           = os.getenv('CM_ML_MODEL_USE_DLA', 'NO') in ('YES', 'yes', 'ON', 'on', '1')
+MODEL_MAX_BATCH_SIZE    = int(os.getenv('CM_ML_MODEL_MAX_BATCH_SIZE', BATCH_SIZE))
 
 
 ## Internal processing:
@@ -36,17 +36,17 @@ INTERMEDIATE_DATA_TYPE  = np.float32    # default for internal conversion
 
 ## Image normalization:
 #
-MODEL_NORMALIZE_DATA    = os.getenv('ML_MODEL_NORMALIZE_DATA') in ('YES', 'yes', 'ON', 'on', '1')
-MODEL_NORMALIZE_LOWER   = float(os.getenv('ML_MODEL_NORMALIZE_LOWER', -1.0))
-MODEL_NORMALIZE_UPPER   = float(os.getenv('ML_MODEL_NORMALIZE_UPPER',  1.0))
-SUBTRACT_MEAN           = os.getenv('ML_MODEL_SUBTRACT_MEAN', 'YES') in ('YES', 'yes', 'ON', 'on', '1')
-GIVEN_CHANNEL_MEANS     = os.getenv('ML_MODEL_GIVEN_CHANNEL_MEANS', '')
+MODEL_NORMALIZE_DATA    = os.getenv('CM_ML_MODEL_NORMALIZE_DATA') in ('YES', 'yes', 'ON', 'on', '1')
+MODEL_NORMALIZE_LOWER   = float(os.getenv('CM_ML_MODEL_NORMALIZE_LOWER', -1.0))
+MODEL_NORMALIZE_UPPER   = float(os.getenv('CM_ML_MODEL_NORMALIZE_UPPER',  1.0))
+SUBTRACT_MEAN           = os.getenv('CM_ML_MODEL_SUBTRACT_MEAN', 'YES') in ('YES', 'yes', 'ON', 'on', '1')
+GIVEN_CHANNEL_MEANS     = os.getenv('CM_ML_MODEL_GIVEN_CHANNEL_MEANS', '')
 if GIVEN_CHANNEL_MEANS:
     GIVEN_CHANNEL_MEANS = np.fromstring(GIVEN_CHANNEL_MEANS, dtype=np.float32, sep=' ').astype(INTERMEDIATE_DATA_TYPE)
     if MODEL_COLOURS_BGR:
         GIVEN_CHANNEL_MEANS = GIVEN_CHANNEL_MEANS[::-1]     # swapping Red and Blue colour channels
 
-GIVEN_CHANNEL_STDS      = os.getenv('ML_MODEL_GIVEN_CHANNEL_STDS', '')
+GIVEN_CHANNEL_STDS      = os.getenv('CM_ML_MODEL_GIVEN_CHANNEL_STDS', '')
 if GIVEN_CHANNEL_STDS:
     GIVEN_CHANNEL_STDS = np.fromstring(GIVEN_CHANNEL_STDS, dtype=np.float32, sep=' ').astype(INTERMEDIATE_DATA_TYPE)
     if MODEL_COLOURS_BGR:
@@ -56,15 +56,15 @@ if GIVEN_CHANNEL_STDS:
 
 ## ImageNet dataset properties:
 #
-LABELS_PATH             = os.environ['CK_CAFFE_IMAGENET_SYNSET_WORDS_TXT']
+LABELS_PATH             = os.environ['CM_CAFFE_IMAGENET_SYNSET_WORDS_TXT']
 
 
 ## Preprocessed input images' properties:
 #
-IMAGE_DIR               = os.getenv('CK_ENV_DATASET_IMAGENET_PREPROCESSED_DIR')
-IMAGE_LIST_FILE_NAME    = os.getenv('CK_ENV_DATASET_IMAGENET_PREPROCESSED_SUBSET_FOF')
+IMAGE_DIR               = os.getenv('CM_DATASET_IMAGENET_PREPROCESSED_DIR')
+IMAGE_LIST_FILE_NAME    = os.getenv('CM_DATASET_IMAGENET_PREPROCESSED_SUBSET_FOF')
 IMAGE_LIST_FILE         = os.path.join(IMAGE_DIR, IMAGE_LIST_FILE_NAME)
-IMAGE_DATA_TYPE         = os.getenv('CK_ENV_DATASET_IMAGENET_PREPROCESSED_DATA_TYPE', 'float32')
+IMAGE_DATA_TYPE         = os.getenv('CM_DATASET_IMAGENET_PREPROCESSED_DATA_TYPE', 'float32')
 IMAGE_DATA_TYPE         = "uint8"
 
 
