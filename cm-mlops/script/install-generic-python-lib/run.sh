e26bf82dd1c298ea680b0a9003622623eef44827
Arjun Suresh <arjunsuresh1987@gmail.com>
2022-10-21 07:36:22 +0100

    Fixed tensorflow path for generic-python-lib-install

--- cm-mlops/script/install-generic-python-lib/run.sh
+++ cm-mlops/script/install-generic-python-lib/run.sh
@@ -2,12 +2,12 @@
 
 if [[ ${CM_PYTHON_PACKAGE_NAME}  == "tensorflow" ]]; then
     if [[ ${CM_HOST_OS_FLAVOR} == "macos" ]]; then
-        . tensorflow/run-macos.sh
+        . ${CM_TMP_CURRENT_SCRIPT_PATH}/tensorflow/run-macos.sh
         test $? -eq 0 || exit 1
         exit 0
     fi
     if [[ ${CM_HOST_PLATFORM_FLAVOR} == "aarch64" ]]; then
-        . tensorflow/run-aarch64.sh
+        . ${CM_TMP_CURRENT_SCRIPT_PATH}/tensorflow/run-aarch64.sh
         test $? -eq 0 || exit 1
         exit 0
     fi
