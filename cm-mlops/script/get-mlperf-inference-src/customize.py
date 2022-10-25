dca2d7b582c543d7e2992a759bc82d4178aaaf6d
Grigori Fursin <Grigori.Fursin@cTuning.org>
2022-10-25 23:42:43 +0200

    added support for any version (checkout) in get-mlperf-inference-src

--- cm-mlops/script/get-mlperf-inference-src/customize.py
+++ cm-mlops/script/get-mlperf-inference-src/customize.py
@@ -10,14 +10,22 @@ def preprocess(i):
         return {'return':1, 'error': 'Windows is not supported in this script yet'}
 
     env = i['env']
+    meta = i['meta']
 
     if 'CM_GIT_DEPTH' not in env:
         env['CM_GIT_DEPTH'] = ''
-    
+
     if 'CM_GIT_RECURSE_SUBMODULES' not in env:
         env['CM_GIT_RECURSE_SUBMODULES'] = ''
 
+    need_version = env.get('CM_VERSION','')
+    versions = meta['versions']
+
+    if need_version!='' and not need_version in versions:
+        env['CM_GIT_CHECKOUT'] = need_version
+
     return {'return':0}
+
 
 def postprocess(i):
 
@@ -37,18 +45,27 @@ def postprocess(i):
     env['+PYTHONPATH'].append(os.path.join(env['CM_MLPERF_INFERENCE_SOURCE'], 'tools', 'submission'))
 
     valid_models = get_valid_models(env['CM_MLPERF_LAST_RELEASE'], env['CM_MLPERF_INFERENCE_SOURCE'])
+
     state['CM_MLPERF_INFERENCE_MODELS'] = valid_models
 
     return {'return':0}
 
+
 def get_valid_models(mlperf_version, mlperf_path):
+
     import sys
+
     submission_checker_dir = os.path.join(mlperf_path, "tools", "submission")
+
     sys.path.append(submission_checker_dir)
     if not os.path.exists(os.path.join(submission_checker_dir, "submission_checker.py")):
         shutil.copy(os.path.join(submission_checker_dir,"submission-checker.py"), os.path.join(submission_checker_dir,
         "submission_checker.py"))
+
     import submission_checker as checker
+
     config = checker.MODEL_CONFIG
+
     valid_models = config[mlperf_version]["models"]
+
     return valid_models
