04d362279746db4917fb6663d13d718326034dce
Grigori Fursin <Grigori.Fursin@cTuning.org>
2022-10-25 16:42:48 +0200

    adding docker containers

--- docker/cm-rhel-cpu-147f9afcc2774484-build.bat
+++ docker/cm-rhel-cpu-147f9afcc2774484-build.bat
@@ -1,14 +1,14 @@
 rem ubuntu [18.04 ; 20.04 ; 22.04]
 rem debian [9 ; 10]
 
-set CM_OS_NAME=ubuntu
-set CM_OS_VERSION=22.04
+set CM_OS_NAME=rhel
+set CM_OS_VERSION=9
 
 rem set CM_OS_NAME=debian
 rem set CM_OS_VERSION=10
 
-docker build -f cm-ubuntu-debian-cpu-2894f51d6a11479d.Dockerfile ^
-   -t ckrepo/cm-ubuntu-debian-cpu-2894f51d6a11479d:ubuntu-22.04 ^
+docker build -f cm-rhel-cpu-147f9afcc2774484.Dockerfile ^
+   -t ckrepo/cm-rhel-cpu-147f9afcc2774484:%CM_OS_NAME%-%CM_OS_VERSION% ^
    --build-arg cm_os_name=%CM_OS_NAME% ^
    --build-arg cm_os_version=%CM_OS_VERSION% ^
    .
