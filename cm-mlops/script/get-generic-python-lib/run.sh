aea6a8198399a2cbaae262b0b78aab23d6c6af6c
Grigori Fursin <gfursin@gmail.com>
2022-10-26 17:53:01 +0000

    clean up

--- cm-mlops/script/get-generic-python-lib/run.sh
+++ cm-mlops/script/get-generic-python-lib/run.sh
@@ -3,8 +3,3 @@
 CM_TMP_CURRENT_SCRIPT_PATH=${CM_TMP_CURRENT_SCRIPT_PATH:-$PWD}
 
 ${CM_PYTHON_BIN_WITH_PATH} ${CM_TMP_CURRENT_SCRIPT_PATH}/detect-version.py > tmp-ver.out 2> tmp-ver.err
-
-echo "********************************"
-cat tmp-ver.out
-cat tmp-ver.err
-echo "********************************"
