1f2469e0b8bba4d2d6ae7649bc1120acb264fbda
Arjun Suresh <arjunsuresh1987@gmail.com>
2022-10-26 18:20:29 +0100

    Added run.sh for openimages-accuracy script

--- cm-mlops/script/run-accuracy-open-images/customize.py
+++ cm-mlops/script/run-accuracy-open-images/customize.py
@@ -10,17 +10,25 @@ def preprocess(i):
     if results_dir == "":
         print("Please set CM_MLPERF_ACCURACY_RESULTS_DIR")
         return {'return':-1}
-
+    run_cmds = []
     results_dir_split = results_dir.split(":")
     for result_dir in results_dir_split:
-        CMD = env['CM_PYTHON_BIN'] + " -m pip install --user pycocotools && " + env['CM_PYTHON_BIN'] + " '" + os.path.join(env['CM_MLPERF_INFERENCE_VISION_PATH'], "tools", \
+        CMD = env['CM_PYTHON_BIN'] + " '" + os.path.join(env['CM_MLPERF_INFERENCE_VISION_PATH'], "tools", \
             "accuracy-openimages.py") + "' --mlperf-accuracy-file '" + os.path.join(result_dir, \
                     "mlperf_log_accuracy.json") + "' --openimages-dir '" + env['CM_DATASET_PATH'] + "' > '" + \
             os.path.join(result_dir, "accuracy.txt") + "'"
-        print(CMD)
-        ret = os.system(CMD)
-        print("\n")
+        run_cmds.append(CMD)
+
+    env['RUN_CMDS'] = "??".join(run_cmds)
+    return {'return':0}
+
+def postprocess(i):
+
+    os_info = i['os_info']
+    env = i['env']
+    results_dir = env.get("CM_MLPERF_ACCURACY_RESULTS_DIR", "")
+    for result_dir in results_dir_split:
         with open(os.path.join(result_dir, "accuracy.txt"), "r") as fp:
             print(fp.read())
+    return {'return':0}
 
-    return {'return':ret}
