67f4e301b0224080960596f4daa86bdcbd9554cb
Arjun Suresh <arjunsuresh1987@gmail.com>
2022-10-28 16:30:29 +0100

    Support detecting imagenet preprocessed dataset

--- cm-mlops/script/get-preprocessed-dataset-imagenet/run.sh
+++ cm-mlops/script/get-preprocessed-dataset-imagenet/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+if [ ! -z ${CM_IMAGENET_PREPROCESSED_PATH+x} ]; then
+    exit 0
+fi
 ${CM_PYTHON_BIN} -m pip install -r ${CM_TMP_CURRENT_SCRIPT_PATH}/requirements.txt
 ${CM_PYTHON_BIN} ${CM_TMP_CURRENT_SCRIPT_PATH}/preprocess.py
