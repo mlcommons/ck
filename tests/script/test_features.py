b278d31f94211f953d1163f8083cf8c57b637331
Grigori Fursin <Grigori.Fursin@cTuning.org>
2022-11-11 15:43:29 +0100

    fixed venv

--- tests/script/test_features.py
+++ tests/script/test_features.py
@@ -5,5 +5,5 @@ import check as checks
 
 r = cm.access({'action':'run', 'automation':'script', 'tags': 'install,python-venv', 'env': {'CM_NAME': 'test'}, 'quiet': 'yes'})
 checks.check_return(r)
-r = cm.access({'action':'search', 'automation': 'cache', 'tags': 'get,python-venv-test'})
+r = cm.access({'action':'search', 'automation': 'cache', 'tags': 'get,venv-test'})
 checks.check_list(r, "get,python-venv")
