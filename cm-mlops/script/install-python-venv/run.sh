c844800a39521dfe3921d904e1c29bd59d3ce9ab
Grigori Fursin <Grigori.Fursin@cTuning.org>
2022-11-23 10:01:10 +0100

    use user-specified name for the python venv directory: cm run script "install python-venv" --name=XYZ will use "xyz" as a virtual env directory name instead of "venv" ...

--- cm-mlops/script/install-python-venv/run.sh
+++ cm-mlops/script/install-python-venv/run.sh
@@ -3,5 +3,5 @@
 ${CM_PYTHON_BIN_WITH_PATH} -m pip install virtualenv
 test $? -eq 0 || exit 1
 
-${CM_PYTHON_BIN_WITH_PATH} -m venv venv
+${CM_PYTHON_BIN_WITH_PATH} -m venv ${CM_VIRTUAL_ENV_DIR}
 test $? -eq 0 || exit 1
