0a12c839b439b4b9941154c8c3c07a30fbdb75b9
Arjun Suresh <arjunsuresh1987@gmail.com>
2022-12-06 16:16:18 +0000

    CM_DOCKER_IMAGE_RUN_ -> CM_DOCKER_RUN_

--- cm-mlops/script/run-docker-container/customize.py
+++ cm-mlops/script/run-docker-container/customize.py
@@ -25,7 +25,7 @@ def preprocess(i):
         raise Exception('CM script with tags '+ env['CM_DOCKER_RUN_SCRIPT_TAGS'] + ' not found!')
     PATH = r['list'][0].path
     os.chdir(PATH)
-    env['CM_DOCKER_IMAGE_RUN_CMD'] = CM_RUN_CMD
+    env['CM_DOCKER_RUN_CMD'] = CM_RUN_CMD
     DOCKER_CONTAINER = env['CM_DOCKER_IMAGE_REPO'] +  ":" + env['CM_DOCKER_IMAGE_TAG'] 
 
     CMD = "docker images -q " +  DOCKER_CONTAINER + " 2> /dev/null"
