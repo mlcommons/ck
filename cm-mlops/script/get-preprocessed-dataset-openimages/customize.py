7128ad0421668c287ebe691c5774ec4041c4a23c
Arjun Suresh <arjunsuresh1987@gmail.com>
2022-12-16 11:27:01 +0000

    Override dataset_preprocessed_path for openimages

--- cm-mlops/script/get-preprocessed-dataset-openimages/customize.py
+++ cm-mlops/script/get-preprocessed-dataset-openimages/customize.py
@@ -11,3 +11,10 @@ def preprocess(i):
     print("Using MLCommons Inference source from '" + env['CM_MLPERF_INFERENCE_SOURCE'] +"'")
 
     return {'return': 0}
+
+def postprocess(i):
+    env = i['env']
+    env['CM_DATASET_ANNOTATIONS_PATH'] = os.path.join(os.getcwd(), "annotations")
+    env['CM_DATASET_PREPROCESSED_PATH'] = os.path.join(env['CM_DATASET_PREPROCESSED_PATH'], "validation", "data")
+
+    return {'return': 0}
