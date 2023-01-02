728e4d311bd47a35969288c1ef309acd4f8c552b
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-01-02 18:23:34 +0000

    Fix resnet50 tflite model URLs, fixes #611

--- cm-mlops/script/get-ml-model-resnet50/customize.py
+++ cm-mlops/script/get-ml-model-resnet50/customize.py
@@ -19,21 +19,19 @@ def preprocess(i):
 
     print ('Downloading from {}'.format(url))
 
-    if url.endswith(".gz") or url.endswith(".tflite"):
-        env['CM_TMP_WGET'] = "yes"
-        if url.endswith(".tflite"):
-            env['CM_ML_MODEL_FILE']=os.path.basename(url)
-        elif url.endswith(".gz"):
-            env['CM_TMP_EXTRACT'] = "yes"
-            env['CM_TMP_EXTRACT_FILE_NAME'] = os.path.basename(url)
-            env['CM_ML_MODEL_FILE']=env['CM_TMP_EXTRACT_FILE_NAME'][:-3]
-        env['CM_ML_MODEL_FILE_WITH_PATH']=os.path.join(os.getcwd(), env['CM_ML_MODEL_FILE'])
+    r = cm.access({'action':'download_file',
+                'automation':'utils,dc2743f8450541e3',
+                'url':url})
+    if r['return']>0: return r
+
+    filename = r['filename']
+
+    if env.get('CM_UNZIP') == "yes":
+        os.system("unzip "+filename)
+        filename = env['CM_ML_MODEL_FILE']
+        env['CM_ML_MODEL_FILE_WITH_PATH']=os.path.join(path, filename)
     else:
-        r = cm.access({'action':'download_file', 
-                   'automation':'utils,dc2743f8450541e3', 
-                   'url':url})
-        if r['return']>0: return r
-        env['CM_ML_MODEL_FILE']=r['filename']
+        env['CM_ML_MODEL_FILE'] = filename
         env['CM_ML_MODEL_FILE_WITH_PATH']=r['path']
 
     # Add to path
