197c70957fab79ca0a51c30f5d217396f71885e2
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-01-22 19:38:05 +0000

    Fix syntax for tutorial-test

--- tests/script/test_tutorials.py
+++ tests/script/test_tutorials.py
@@ -5,12 +5,12 @@ import check as checks
 
 r = cm.access({'action':'run', 'automation':'script', 'tags': 'app,mlperf,inference,generic,_cpp,_retinanet,_onnxruntime,_cpu', 'adr': \
     {'python': {'version_min': '3.8'}, 'compiler': {'tags': "gcc"}, 'openimages-preprocessed': {'tags': '_500'}}, 'scenario': 'Offline', \
-    'mode': 'accuracy', 'test_query_count': '10', 'rerun', 'quiet': 'yes'})
+    'mode': 'accuracy', 'test_query_count': '10', 'rerun': 'true', 'quiet': 'yes'})
 checks.check_return(r)
 
 r = cm.access({'action':'run', 'automation':'script', 'tags': 'app,mlperf,inference,generic,_cpp,_retinanet,_onnxruntime,_cpu', 'adr': \
     {'python': {'version_min': '3.8'}, 'compiler': {'tags': "gcc"}, 'openimages-preprocessed': {'tags': '_500'}}, 'scenario': 'Offline', \
-    'mode': 'performance', 'test_query_count': '10', 'rerun', 'quiet': 'yes'})
+    'mode': 'performance', 'test_query_count': '10', 'rerun': 'true', 'quiet': 'yes'})
 checks.check_return(r)
 
 r = cm.access({'action':'run', 'automation':'script', 'tags': 'install,python-venv', 'version': '3.10.8', 'name': 'mlperf' })
@@ -19,24 +19,24 @@ checks.check_return(r)
 r = cm.access({'action':'run', 'automation':'script', 'tags': 'run,mlperf,inference,generate-run-cmds,_submission,_short,_dashboard', 'adr': \
         {'python': {'name': 'mlperf', 'version_min': '3.8'}, 'compiler': {'tags': "gcc"}, 'openimages-preprocessed': {'tags': '_500'}}, 'submitter': 'Community', \
         'implementation': 'cpp', 'hw_name': 'default', 'model': 'retinanet', 'backend': 'onnxruntime', 'device': 'cpu', 'scenario': 'Offline', \
-        'test_query_count': '10', 'clean', 'quiet': 'yes'})
+        'test_query_count': '10', 'clean': 'true', 'quiet': 'yes'})
 checks.check_return(r)
 
 r = cm.access({'action':'run', 'automation':'script', 'tags': 'run,mlperf,inference,generate-run-cmds', 'adr': \
         {'python': {'name': 'mlperf', 'version_min': '3.8'}}, 'submitter': 'Community', \
         'implementation': 'python', 'hw_name': 'default', 'model': 'resnet50', 'backend': 'tvm-onnx', 'device': 'cpu', 'scenario': 'Offline', \
-        'mode': 'accuracy', 'test_query_count': '5', 'clean', 'quiet': 'yes'})
+        'mode': 'accuracy', 'test_query_count': '5', 'clean': 'true', 'quiet': 'yes'})
 checks.check_return(r)
 
 r = cm.access({'action':'run', 'automation':'script', 'tags': 'run,mlperf,inference,generate-run-cmds', 'adr': \
         {'python': {'name': 'mlperf', 'version_min': '3.8'}, 'tvm': {'tags': '_pip-install'}}, 'submitter': 'Community', \
         'implementation': 'python', 'hw_name': 'default', 'model': 'resnet50', 'backend': 'tvm-onnx', 'device': 'cpu', 'scenario': 'Offline', \
-        'mode': 'accuracy', 'test_query_count': '5', 'clean', 'quiet': 'yes'})
+        'mode': 'accuracy', 'test_query_count': '5', 'clean': 'true', 'quiet': 'yes'})
 checks.check_return(r)
 
 r = cm.access({'action':'run', 'automation':'script', 'tags': 'run,mlperf,inference,generate-run-cmds,_submission,_dashboard', 'adr': \
         {'python': {'name': 'mlperf', 'version_min': '3.8'}}, 'submitter': 'Community', \
         'implementation': 'python', 'hw_name': 'default', 'model': 'resnet50', 'backend': 'tvm-onnx', 'device': 'cpu', 'scenario': 'Offline', \
-        'test_query_count': '500', 'clean', 'quiet': 'yes'})
+        'test_query_count': '500', 'clean': 'true', 'quiet': 'yes'})
 checks.check_return(r)
 
