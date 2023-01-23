a39787a14d17e282957141089afe362b6591c33d
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-01-23 09:55:18 +0000

    Fix imports for tutorial tests

--- tests/tutorials/test_tutorial_retinanet.py
+++ tests/tutorials/test_tutorial_retinanet.py
@@ -1,9 +1,11 @@
 # This test covers version, variation, compilation from src, add_deps, add_deps_recursive, deps, post_deps
 
 import cmind as cm
+from pathlib import Path
 import sys
+import os
 
-sys.path.insert(1, "../script")
+sys.path.insert(1, os.path.join(Path(__file__).parent.parent.resolve(), "script"))
 import check as checks
 
 r = cm.access({'action':'run', 'automation':'script', 'tags': 'app,mlperf,inference,generic,_cpp,_retinanet,_onnxruntime,_cpu', 'adr': \
@@ -24,22 +26,3 @@ r = cm.access({'action':'run', 'automation':'script', 'tags': 'run,mlperf,infere
         'implementation': 'cpp', 'hw_name': 'default', 'model': 'retinanet', 'backend': 'onnxruntime', 'device': 'cpu', 'scenario': 'Offline', \
         'test_query_count': '10', 'clean': 'true', 'quiet': 'yes'})
 checks.check_return(r)
-
-r = cm.access({'action':'run', 'automation':'script', 'tags': 'run,mlperf,inference,generate-run-cmds', 'adr': \
-        {'python': {'name': 'mlperf', 'version_min': '3.8'}}, 'submitter': 'Community', \
-        'implementation': 'python', 'hw_name': 'default', 'model': 'resnet50', 'backend': 'tvm-onnx', 'device': 'cpu', 'scenario': 'Offline', \
-        'mode': 'accuracy', 'test_query_count': '5', 'clean': 'true', 'quiet': 'yes'})
-checks.check_return(r)
-
-r = cm.access({'action':'run', 'automation':'script', 'tags': 'run,mlperf,inference,generate-run-cmds', 'adr': \
-        {'python': {'name': 'mlperf', 'version_min': '3.8'}, 'tvm': {'tags': '_pip-install'}}, 'submitter': 'Community', \
-        'implementation': 'python', 'hw_name': 'default', 'model': 'resnet50', 'backend': 'tvm-onnx', 'device': 'cpu', 'scenario': 'Offline', \
-        'mode': 'accuracy', 'test_query_count': '5', 'clean': 'true', 'quiet': 'yes'})
-checks.check_return(r)
-
-r = cm.access({'action':'run', 'automation':'script', 'tags': 'run,mlperf,inference,generate-run-cmds,_submission,_dashboard', 'adr': \
-        {'python': {'name': 'mlperf', 'version_min': '3.8'}}, 'submitter': 'Community', \
-        'implementation': 'python', 'hw_name': 'default', 'model': 'resnet50', 'backend': 'tvm-onnx', 'device': 'cpu', 'scenario': 'Offline', \
-        'test_query_count': '500', 'clean': 'true', 'quiet': 'yes'})
-checks.check_return(r)
-
--- tests/tutorials/test_tutorial_tvm.py
+++ tests/tutorials/test_tutorial_tvm.py
@@ -1,11 +1,13 @@
 # This test covers version, variation, compilation from src, add_deps, add_deps_recursive, deps, post_deps
 
 import cmind as cm
+
+from pathlib import Path
 import sys
+import os
 
-sys.path.insert(1, "../script")
+sys.path.insert(1, os.path.join(Path(__file__).parent.parent.resolve(), "script"))
 import check as checks
-
 
 r = cm.access({'action':'run', 'automation':'script', 'tags': 'run,mlperf,inference,generate-run-cmds', 'adr': \
         {'python': {'name': 'mlperf', 'version_min': '3.8'}}, 'submitter': 'Community', \
