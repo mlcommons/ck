b3b7110fe214113e77323adae0d7feabe5747045
Grigori Fursin <Grigori.Fursin@cTuning.org>
2023-01-23 16:58:01 +0100

    improved GUI

--- cm-mlops/script/gui/app.py
+++ cm-mlops/script/gui/app.py
@@ -10,7 +10,10 @@ def main():
     
     script_path = os.environ.get('CM_GUI_SCRIPT_PATH','')
     script_alias = os.environ.get('CM_GUI_SCRIPT_ALIAS','')
+    title = os.environ.get('CM_GUI_TITLE', '')
+    no_run = os.environ.get('CM_GUI_NO_RUN', '')
 
+    # Check if script tags are specified from CMD
     script_tags = ''
     script_tags_from_url = query_params.get('tags',[''])
 
@@ -39,8 +42,16 @@ def main():
     if script_tags == '':
         script_tags = os.environ.get('CM_GUI_SCRIPT_TAGS','')
 
-    title = os.environ.get('CM_GUI_TITLE', '')
-    no_run = os.environ.get('CM_GUI_NO_RUN', '')
+    # Read meta
+    meta = {}
+    if script_path!='' and os.path.isdir(script_path):
+        fn = os.path.join(script_path, '_cm')
+        r = cmind.utils.load_yaml_and_json(fn)
+        if r['return'] == 0:
+            meta = r['meta']
+
+    if meta.get('gui_title','')!='':
+        title = meta['gui_title']
 
     # Set title
     if title=='':
@@ -62,116 +73,110 @@ def main():
 
     st_variations = {}
 
-    meta = {}
-    if script_path!='' and os.path.isdir(script_path):
-        fn = os.path.join(script_path, '_cm')
-        r = cmind.utils.load_yaml_and_json(fn)
-        if r['return'] == 0:
-            meta = r['meta']
+    if len(meta)>0:
+        variations = meta.get('variations',{})
 
-            variations = meta.get('variations',{})
+        default_variation = meta.get('default_variation','')
 
-            default_variation = meta.get('default_variation','')
+        variation_keys = sorted(list(variations.keys()))
 
-            variation_keys = sorted(list(variations.keys()))
+        for variation_key in sorted(variation_keys):
+            variation = variations[variation_key]
 
-            for variation_key in sorted(variation_keys):
-                variation = variations[variation_key]
+            alias = variation.get('alias','').strip()
 
-                alias = variation.get('alias','').strip()
+            if alias!='':
+                aliases = variation_alias.get(alias, [])
+                if variation_key not in aliases: 
+                    aliases.append(variation_key)
+                variation_alias[alias]=aliases
 
-                if alias!='':
-                    aliases = variation_alias.get(alias, [])
-                    if variation_key not in aliases: 
-                        aliases.append(variation_key)
-                    variation_alias[alias]=aliases
+                # Do not continue this loop if alias
+                continue
 
-                    # Do not continue this loop if alias
-                    continue
+            default = variation.get('default', False)
 
-                default = variation.get('default', False)
+            if not default:
+                # Check outdated
+                if default_variation == variation_key:
+                    default = True
 
-                if not default:
-                    # Check outdated
-                    if default_variation == variation_key:
-                        default = True
+            extra1 = ''
+            extra2 = ''
+            if default:
+                extra1 = '**'
+                extra2 = '** (default)'
 
-                extra1 = ''
-                extra2 = ''
-                if default:
-                    extra1 = '**'
-                    extra2 = '** (default)'
+                default_variations.append(variation_key)
 
-                    default_variations.append(variation_key)
+            group = variation.get('group','')
 
-                group = variation.get('group','')
+            if variation_key.endswith('_'):
+                group = '*internal*'
+            elif group == '':
+                group = '*no-group*'
 
-                if variation_key.endswith('_'):
-                    group = '*internal*'
-                elif group == '':
-                    group = '*no-group*'
+            if group not in variation_groups:
+                variation_groups[group]=[]
 
-                if group not in variation_groups:
-                    variation_groups[group]=[]
+            variation_groups[group].append(variation_key)
 
-                variation_groups[group].append(variation_key)
-
-            # Prepare variation_groups
+        # Prepare variation_groups
 #            st.markdown("""---""")
-            st.subheader('Script variations')
+        st.subheader('Script variations')
 
-            variation_groups_order = meta.get('variation_groups_order',[])
-            for variation in sorted(variation_groups):
-                if variation not in variation_groups_order:
-                    variation_groups_order.append(variation)
+        variation_groups_order = meta.get('variation_groups_order',[])
+        for variation in sorted(variation_groups):
+            if variation not in variation_groups_order:
+                variation_groups_order.append(variation)
 
-            for group_key in variation_groups_order:
-                group_key_cap = group_key.replace('-',' ').capitalize()
-                if not group_key.startswith('*'):
-                    y = ['']
+        for group_key in variation_groups_order:
+            group_key_cap = group_key.replace('-',' ').capitalize()
+            if not group_key.startswith('*'):
+                y = ['']
 
-                    index = 0
-                    selected_index = 0
-                    for variation_key in sorted(variation_groups[group_key]):
-                        index += 1
-                        y.append(variation_key)
-                        if variation_key in default_variations:
-                            selected_index=index
+                index = 0
+                selected_index = 0
+                for variation_key in sorted(variation_groups[group_key]):
+                    index += 1
+                    y.append(variation_key)
+                    if variation_key in default_variations:
+                        selected_index=index
 
-                    st_variations[group_key] = st.selectbox(group_key_cap, sorted(y), index=selected_index, key=group_key)
-                elif group_key == '*no-group*':
-                    for variation_key in sorted(variation_groups[group_key]):
-                        st_variations[variation_key] = st.checkbox(variation_key.capitalize(), key=variation_key)
+                st_variations[group_key] = st.selectbox(group_key_cap, sorted(y), index=selected_index, key=group_key)
+            elif group_key == '*no-group*':
+                for variation_key in sorted(variation_groups[group_key]):
+                    st_variations[variation_key] = st.checkbox(variation_key.capitalize(), key=variation_key)
 
 
-            # Prepare inputs
-            input_desc=meta.get('input_description',{})
+        # Prepare inputs
+        input_desc=meta.get('input_description',{})
 
-            if len(input_desc)>0:
-                st.markdown("""---""")
-                st.subheader('Script flags')
-                
-                for key in input_desc:
-                    value = input_desc[key]
+        if len(input_desc)>0:
+            st.markdown("""---""")
+            st.subheader('Script flags')
+            
+            for key in input_desc:
+                value = input_desc[key]
 
-                    if type(value) == dict:
-                        desc = value['desc']
+                if type(value) == dict:
+                    desc = value['desc']
 
-                        choices = value.get('choices', [])
-                        boolean = value.get('boolean', False)
-                        default = value.get('default', '')
+                    choices = value.get('choices', [])
+                    boolean = value.get('boolean', False)
+                    default = value.get('default', '')
 
-                        if boolean:
-                            st_inputs[key] = st.checkbox(desc, value=default, key=key)
-                        elif len(choices)>0:
-                            selected_index = choices.index(default) if default!='' else 0
-                            st_inputs[key] = st.selectbox(desc, choices, index=selected_index, key=key)
-                        else:
-                            st_inputs[key] = st.text_input(desc, value=default, key=key)
-                            
+                    if boolean:
+                        st_inputs[key] = st.checkbox(desc, value=default, key=key)
+                    elif len(choices)>0:
+                        selected_index = choices.index(default) if default!='' else 0
+                        st_inputs[key] = st.selectbox(desc, choices, index=selected_index, key=key)
                     else:
-                        desc = value
-                        st_inputs[key] = st.text_input(desc)
+                        st_inputs[key] = st.text_input(desc, value=default, key=key)
+                        
+                else:
+                    desc = value
+                    st_inputs[key] = st.text_input(desc)
 
 
     # Check tags
