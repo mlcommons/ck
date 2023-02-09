88a40697ffe9d53693bc6a90b0e47d652894f8ab
Arjun <arjunsuresh1987@gmail.com>
2023-02-09 12:52:04 +0000

    armnn variation added to tflite-cpp

--- cm-mlops/script/app-mlperf-inference-tflite-cpp/customize.py
+++ cm-mlops/script/app-mlperf-inference-tflite-cpp/customize.py
@@ -23,7 +23,7 @@ def preprocess(i):
     source_files = []
     script_path = i['run_script_input']['path']
 
-    env['CM_SOURCE_FOLDER_PATH'] = os.path.join(script_path, "src")
+    env['CM_SOURCE_FOLDER_PATH'] = os.path.join(script_path, env['CM_TMP_SRC_FOLDER'])
 
     for file in os.listdir(env['CM_SOURCE_FOLDER_PATH']):
         if file.endswith(".c") or file.endswith(".cpp"):
