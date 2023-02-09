fb68b60b7c0b62dd15a84c9a10478304b92416db
Arjun <arjunsuresh1987@gmail.com>
2023-02-09 22:41:11 +0000

    Added support to copy default hw_config file

--- cm-mlops/script/get-mlperf-inference-sut-description/customize.py
+++ cm-mlops/script/get-mlperf-inference-sut-description/customize.py
@@ -40,73 +40,73 @@ def preprocess(i):
             default_hw_path = os.path.join(path, "hardware", "default.json")
             print("HW description file for " + hw_name + " not found. Copying from default!!!")
             shutil.copy(default_hw_path, hw_path)
+
+        state['CM_HW_META'] = json.load(open(hw_path))
+        state['CM_SUT_META'] = state['CM_HW_META']
+        state['CM_SUT_META']['framework'] = backend_desc
+        os_name = env.get('CM_HOST_OS_FLAVOR', '').capitalize()
+        os_version = env.get('CM_HOST_OS_VERSION', '')
+        if os_name and os_version:
+            os_name_string = os_name + " " + os_version
         else:
-            state['CM_HW_META'] = json.load(open(hw_path))
-            state['CM_SUT_META'] = state['CM_HW_META']
-            state['CM_SUT_META']['framework'] = backend_desc
-            os_name = env.get('CM_HOST_OS_FLAVOR', '').capitalize()
-            os_version = env.get('CM_HOST_OS_VERSION', '')
-            if os_name and os_version:
-                os_name_string = os_name + " " + os_version
-            else:
-                os_name_string = ''
-            os_type = env.get('CM_HOST_OS_TYPE', '')
-            kernel = env.get('CM_HOST_OS_KERNEL_VERSION', '')
-            if os_type and kernel:
-                os_name_string += " (" + os_type + "-" + kernel
-                glibc_version = env.get('CM_HOST_OS_GLIBC_VERSION', '')
-                if glibc_version:
-                    os_name_string += '-glibc' + glibc_version
-                os_name_string += ')'
-            python_version = env.get('CM_PYTHON_VERSION', '')
-            compiler = env.get('CM_COMPILER_FAMILY', '')
-            compiler_version = env.get('CM_COMPILER_VERSION', '')
-            state['CM_SUT_META']['submitter'] = submitter
-            state['CM_SUT_META']['operating_system'] = os_name_string
-            state['CM_SUT_META']['other_software_stack'] = "Python: " + python_version + ", " + compiler + "-" + compiler_version
+            os_name_string = ''
+        os_type = env.get('CM_HOST_OS_TYPE', '')
+        kernel = env.get('CM_HOST_OS_KERNEL_VERSION', '')
+        if os_type and kernel:
+            os_name_string += " (" + os_type + "-" + kernel
+            glibc_version = env.get('CM_HOST_OS_GLIBC_VERSION', '')
+            if glibc_version:
+                os_name_string += '-glibc' + glibc_version
+            os_name_string += ')'
+        python_version = env.get('CM_PYTHON_VERSION', '')
+        compiler = env.get('CM_COMPILER_FAMILY', '')
+        compiler_version = env.get('CM_COMPILER_VERSION', '')
+        state['CM_SUT_META']['submitter'] = submitter
+        state['CM_SUT_META']['operating_system'] = os_name_string
+        state['CM_SUT_META']['other_software_stack'] = "Python: " + python_version + ", " + compiler + "-" + compiler_version
 
-            if not state['CM_SUT_META'].get('system_name'):
-                system_name = env.get('CM_MLPERF_SYSTEM_NAME')
-                if not system_name:
-                    system_name = env.get('CM_HOST_SYSTEM_NAME')
-                    if system_name:
-                        system_name+=" (auto detected)"
-                    else:
-                        system_name = " (generic)"
-                state['CM_SUT_META']['system_name'] = system_name
+        if not state['CM_SUT_META'].get('system_name'):
+            system_name = env.get('CM_MLPERF_SYSTEM_NAME')
+            if not system_name:
+                system_name = env.get('CM_HOST_SYSTEM_NAME')
+                if system_name:
+                    system_name+=" (auto detected)"
+                else:
+                    system_name = " (generic)"
+            state['CM_SUT_META']['system_name'] = system_name
 
-            if 'host_processor_core_count' not in state['CM_SUT_META']:
-                physical_cores_per_node = env.get('CM_HOST_CPU_PHYSICAL_CORES_PER_SOCKET')
-                state['CM_SUT_META']['host_processor_core_count'] = physical_cores_per_node
+        if 'host_processor_core_count' not in state['CM_SUT_META']:
+            physical_cores_per_node = env.get('CM_HOST_CPU_PHYSICAL_CORES_PER_SOCKET')
+            state['CM_SUT_META']['host_processor_core_count'] = physical_cores_per_node
 
-            if 'host_processor_model_name' not in state['CM_SUT_META']:
-                state['CM_SUT_META']['host_processor_model_name'] = env.get('CM_HOST_CPU_MODEL_NAME', '')
-            if 'host_processors_per_node' not in state['CM_SUT_META']:
-                state['CM_SUT_META']['host_processors_per_node'] = env.get('CM_HOST_CPU_SOCKETS', '')
+        if 'host_processor_model_name' not in state['CM_SUT_META']:
+            state['CM_SUT_META']['host_processor_model_name'] = env.get('CM_HOST_CPU_MODEL_NAME', '')
+        if 'host_processors_per_node' not in state['CM_SUT_META']:
+            state['CM_SUT_META']['host_processors_per_node'] = env.get('CM_HOST_CPU_SOCKETS', '')
 
-            if 'host_processor_caches' not in state['CM_SUT_META']:
-                state['CM_SUT_META']['host_processor_caches'] = "L1d cache: " + env.get('CM_HOST_CPU_L1D_CACHE_SIZE', ' ') + \
-                        ", L1i cache: " + env.get('CM_HOST_CPU_L1I_CACHE_SIZE', ' ') + ", L2 cache: " + \
-                        env.get('CM_HOST_CPU_L2_CACHE_SIZE', ' ') + \
-                        ", L3 cache: " + env.get('CM_HOST_CPU_L3_CACHE_SIZE', ' ')
+        if 'host_processor_caches' not in state['CM_SUT_META']:
+            state['CM_SUT_META']['host_processor_caches'] = "L1d cache: " + env.get('CM_HOST_CPU_L1D_CACHE_SIZE', ' ') + \
+                    ", L1i cache: " + env.get('CM_HOST_CPU_L1I_CACHE_SIZE', ' ') + ", L2 cache: " + \
+                    env.get('CM_HOST_CPU_L2_CACHE_SIZE', ' ') + \
+                    ", L3 cache: " + env.get('CM_HOST_CPU_L3_CACHE_SIZE', ' ')
 
-            if 'host_processor_frequency' not in state['CM_SUT_META']:
-                state['CM_SUT_META']['host_processor_frequency'] = env.get('CM_HOST_CPU_MAX_MHZ','')
-            if 'host_memory_capacity' not in state['CM_SUT_META']:
-                state['CM_SUT_META']['host_memory_capacity'] = env.get('CM_HOST_MEMORY_CAPACITY', '')
-            if 'host_storage_capacity' not in state['CM_SUT_META']:
-                state['CM_SUT_META']['host_storage_capacity'] = env.get('CM_HOST_DISK_CAPACITY', '')
-            if 'CM_SUT_SW_NOTES' in env:
-                sw_notes = env['CM_SUT_SW_NOTES']
-            else:
-                sw_notes = ''
-            state['CM_SUT_META']['sw_notes'] = sw_notes
+        if 'host_processor_frequency' not in state['CM_SUT_META']:
+            state['CM_SUT_META']['host_processor_frequency'] = env.get('CM_HOST_CPU_MAX_MHZ','')
+        if 'host_memory_capacity' not in state['CM_SUT_META']:
+            state['CM_SUT_META']['host_memory_capacity'] = env.get('CM_HOST_MEMORY_CAPACITY', '')
+        if 'host_storage_capacity' not in state['CM_SUT_META']:
+            state['CM_SUT_META']['host_storage_capacity'] = env.get('CM_HOST_DISK_CAPACITY', '')
+        if 'CM_SUT_SW_NOTES' in env:
+            sw_notes = env['CM_SUT_SW_NOTES']
+        else:
+            sw_notes = ''
+        state['CM_SUT_META']['sw_notes'] = sw_notes
 
-            state['CM_SUT_META'] = dict(sorted(state['CM_SUT_META'].items()))
+        state['CM_SUT_META'] = dict(sorted(state['CM_SUT_META'].items()))
 
-            sut_file = open(sut_path, "w")
-            json.dump(state['CM_SUT_META'], sut_file, indent = 4)
-            sut_file.close()
+        sut_file = open(sut_path, "w")
+        json.dump(state['CM_SUT_META'], sut_file, indent = 4)
+        sut_file.close()
 
     return {'return':0}
 
