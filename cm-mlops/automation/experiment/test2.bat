841c3c418c9eb4bbb4b7635eb2e14e15de4d5e65
Grigori Fursin <Grigori.Fursin@cTuning.org>
2023-02-11 13:02:18 +0100

    added automation "project" to keep scripts, notes, notebooks for benchmarking, autotuning, DSE and other public or private projects ...

--- cm-mlops/automation/experiment/test2.bat
+++ cm-mlops/automation/experiment/test2.bat
@@ -1,1 +1,1 @@
-﻿cm run experiment --tags=xyz @test_input.yaml -- echo %%VAR1%% {{VAR1}} {{VAR2}} {{VAR4{['xx','yy','zz']}}}-%%VAR3%% {{CM_EXPERIMENT_PATH3}}
+﻿cm run experiment --tags=xyz @test_input.yaml -- echo %%VAR1%% --batch_size={{VAR1}} {{VAR2}} {{VAR4{['xx','yy','zz']}}}-%%VAR3%% {{CM_EXPERIMENT_PATH3}}
