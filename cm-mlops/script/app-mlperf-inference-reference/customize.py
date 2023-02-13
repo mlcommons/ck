7386f384cc98f7449376d559244c469dd0f43536
Arjun <arjunsuresh1987@gmail.com>
2023-02-13 18:42:18 +0000

    Fix support for custom imagenet model in reference app

--- cm-mlops/script/app-mlperf-inference-reference/customize.py
+++ cm-mlops/script/app-mlperf-inference-reference/customize.py
@@ -69,7 +69,7 @@ def preprocess(i):
 
     env['CM_MLPERF_LOADGEN_EXTRA_OPTIONS'] +=  " --mlperf_conf '" + env['CM_MLPERF_CONF'] + "'"
 
-    env['MODEL_DIR'] = env['CM_ML_MODEL_PATH']
+    env['MODEL_DIR'] = env.get('CM_ML_MODEL_PATH')
 
     RUN_CMD = ""
     state['RUN'] = {}
@@ -129,7 +129,7 @@ def preprocess(i):
     env['CM_MLPERF_RUN_CMD'] = cmd
     env['CM_RUN_DIR'] = os.getcwd()
     env['CM_RUN_CMD'] = cmd
-    env['CK_PROGRAM_TMP_DIR'] = env['CM_ML_MODEL_PATH'] #for tvm
+    env['CK_PROGRAM_TMP_DIR'] = env.get('CM_ML_MODEL_PATH') #for tvm
 
     if env.get('CM_HOST_PLATFORM_FLAVOR','') == "arm64":
         env['CM_HOST_PLATFORM_FLAVOR'] = "aarch64"
@@ -160,8 +160,9 @@ def get_run_cmd_reference(env, scenario_extra_options, mode_extra_options, datas
             return {'return': 1, 'error': 'No valid model file found!'}
 
         env['LOG_PATH'] = env['CM_MLPERF_OUTPUT_DIR']
-        extra_options = " --dataset " + env['CM_MLPERF_VISION_DATASET_OPTION'] + ' --max-batchsize ' +
-        env['CM_MLPERF_MAX_BATCH_SIZE'] + " --dataset-path "+env['CM_DATASET_PREPROCESSED_PATH']+" --model "+env['MODEL_FILE'] + " --preprocessed_dir "+env['CM_DATASET_PREPROCESSED_PATH']
+        extra_options = " --dataset " + env['CM_MLPERF_VISION_DATASET_OPTION'] + ' --max-batchsize ' + env.get('CM_MLPERF_MAX_BATCH_SIZE', '1') + \
+                " --dataset-path "+env['CM_DATASET_PREPROCESSED_PATH']+" --model "+env['MODEL_FILE'] + \
+                " --preprocessed_dir "+env['CM_DATASET_PREPROCESSED_PATH']
         cmd = "cd '" + os.path.join(env['RUN_DIR'],"python") + "' && "+env['CM_PYTHON_BIN_WITH_PATH']+ " main.py "+\
         "--backend "+env['CM_MLPERF_BACKEND']+ " --scenario="+env['CM_MLPERF_LOADGEN_SCENARIO'] + \
             env['CM_MLPERF_LOADGEN_EXTRA_OPTIONS'] + scenario_extra_options + mode_extra_options + dataset_options + extra_options
