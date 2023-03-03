85b1df319e48bb9eeae4b6cb0ba2cccdf8f78ede
Grigori Fursin <gfursin@gmail.com>
2023-03-03 12:09:55 +0000

    fixed model download from HuggingFace

--- cm-mlops/script/get-ml-model-huggingface-zoo/download_model.py
+++ cm-mlops/script/get-ml-model-huggingface-zoo/download_model.py
@@ -1,12 +1,16 @@
-from setfit import SetFitModel
+from huggingface_hub import hf_hub_download
 import os
 
 model_stub= os.environ.get('CM_MODEL_ZOO_STUB', '')
-print(f"Downloading model {model_stub}")
-stub = f"{model_stub}"
-model = SetFitModel.from_pretrained(stub, cache_dir=os.getcwd())
+model_filename= os.environ.get('CM_MODEL_ZOO_FILENAME', 'model.onnx')
 
-model._save_pretrained(stub)
+print("Downloading model: "+model_stub)
+
+downloaded_model_path = hf_hub_download(repo_id=model_stub,
+                                        filename=model_filename,
+                                        cache_dir=os.getcwd(),
+                                        force_filename=model_filename)
 
 with open('tmp-run-env.out', 'w') as f:
-    f.write(f"CM_ML_MODEL_FILE_WITH_PATH={os.path.join(os.getcwd(),stub)}")
+    f.write(f"CM_ML_MODEL_FILE_WITH_PATH={os.path.join(os.getcwd(),model_filename)}")
+
