e458f9fd1b7a436cfea6492a8be393f49ec10a6c
Grigori Fursin <Grigori.Fursin@cTuning.org>
2023-04-12 11:33:01 +0200

    Added IPOL sources downloader

---
+++ cm-mlops/script/get-ipol-src/customize.py
@@ -0,0 +1,50 @@
+from cmind import utils
+import os
+
+def preprocess(i):
+
+    os_info = i['os_info']
+
+    env = i['env']
+
+    automation = i['automation']
+
+    cm = automation.cmind
+
+    path = os.getcwd()
+
+    url = env['CM_IPOL_SRC_URL']
+
+    year = env.get('CM_IPOL_YEAR', '')
+    number = env.get('CM_IPOL_NUMBER', '')
+
+    url = url.replace('{{CM_IPOL_YEAR}}', year).replace('{{CM_IPOL_NUMBER}}', number)
+
+    print ('Downloading from {}'.format(url))
+
+    r = cm.access({'action':'download_file', 
+                   'automation':'utils,dc2743f8450541e3', 
+                   'url':url})
+    if r['return']>0: return r
+
+    filename = r['filename']
+
+    print ('Unzipping file {}'.format(filename))
+
+    r = cm.access({'action':'unzip_file', 
+                   'automation':'utils,dc2743f8450541e3', 
+                   'filename':filename})
+    if r['return']>0: return r
+
+    if os.path.isfile(filename):
+        print ('Removing file {}'.format(filename))
+        os.remove(filename)
+
+    # Get sub-directory from filename
+    ff = os.path.splitext(filename)
+
+    subdir = ff[0]
+
+    env['CM_IPOL_PATH']=os.path.join(path, subdir)
+
+    return {'return':0}
