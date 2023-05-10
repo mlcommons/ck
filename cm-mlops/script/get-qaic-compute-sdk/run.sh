5515a0f693d7f5eb6d88e1143df619cc23ae8f59
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-05-10 11:19:21 +0100

    Fixes for qaic-compute-sdk

--- cm-mlops/script/get-qaic-compute-sdk/run.sh
+++ cm-mlops/script/get-qaic-compute-sdk/run.sh
@@ -2,5 +2,5 @@
 wget https://codelinaro.jfrog.io/artifactory/codelinaro-toolchain-for-hexagon/v15.0.5/clang+llvm-15.0.5-cross-hexagon-unknown-linux-musl.tar.xz
 export HEXAGON_TOOLS_DIR=`pwd`/clang+llvm-15.0.5-cross-hexagon-unknown-linux-musl/x86_64-linux-gnu
 cd ${CM_QAIC_COMPUTE_SDK_PATH}
-./scripts/build.sh --release --install
+./scripts/build.sh --${CM_QAIC_COMPUTE_SDK_INSTALL_MODE} --install
 test $? -eq 0 || exit $?
