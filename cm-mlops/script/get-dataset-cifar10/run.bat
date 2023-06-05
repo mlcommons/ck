e19aac83519c28270ed8a14f910eeecc67e31180
Grigori Fursin <Grigori.Fursin@cTuning.org>
2023-06-05 22:32:30 +0200

    cifar tinymlperf convertion

--- cm-mlops/script/get-dataset-cifar10/run.bat
+++ cm-mlops/script/get-dataset-cifar10/run.bat
@@ -13,3 +13,29 @@ del /Q /S %CM_DATASET_FILENAME1%
 
 echo CM_DATASET_PATH=%CD%\cifar-10-batches-py > tmp-run-env.out
 echo CM_DATASET_CIFAR10_PATH=%CD%\cifar-10-batches-py >> tmp-run-env.out
+
+if "%CM_DATASET_CONVERT_TO_TINYMLPERF%" == "yes" (
+ echo.
+ echo Copying TinyMLPerf convertor ...
+ echo.
+
+ copy /B /Y %CM_MLPERF_TINY_TRAINING_IC%\* .
+ copy /B /Y %CM_MLPERF_TINY_TRAINING_IC%\* .
+
+ echo.
+ echo Installing Python requirements ...
+ echo.
+
+ %CM_PYTHON_BIN% -m pip install -r %CM_TMP_CURRENT_SCRIPT_PATH%\requirements.txt
+ IF %ERRORLEVEL% NEQ 0 EXIT 1
+
+ echo.
+ echo Converting ...
+ echo.
+
+ %CM_PYTHON_BIN% perf_samples_loader.py
+ IF %ERRORLEVEL% NEQ 0 EXIT 1
+
+ echo CM_DATASET_CIFAR10_TINYMLPERF_PATH=%CD%\perf_samples >> tmp-run-env.out
+)
+
--- cm-mlops/script/get-mlperf-tiny-src/customize.py
+++ cm-mlops/script/get-mlperf-tiny-src/customize.py
@@ -25,17 +25,17 @@ def postprocess(i):
 
     env['CM_MLPERF_TINY_SRC'] = os.path.join(os.getcwd(), 'src')
     env['CM_MLPERF_TINY_BENCHMARK'] = os.path.join(os.getcwd(), 'src', 'benchmark')
-    env['CM_MLPERF_TINY_DATASETS'] = os.path.join(os.getcwd(), 'src', 'evaluation', 'datasets')
-    env['CM_MLPERF_TINY_DATASETS_AD'] = os.path.join(os.getcwd(), 'src', 'evaluation', 'datasets', 'ad01')
-    env['CM_MLPERF_TINY_DATASETS_IC'] = os.path.join(os.getcwd(), 'src', 'evaluation', 'datasets', 'ic01')
-    env['CM_MLPERF_TINY_DATASETS_KWS'] = os.path.join(os.getcwd(), 'src', 'evaluation', 'datasets', 'kws01')
-    env['CM_MLPERF_TINY_DATASETS_KWS_OPEN'] = os.path.join(os.getcwd(), 'src', 'evaluation', 'datasets', 'kws01-open')
-    env['CM_MLPERF_TINY_DATASETS_VWW'] = os.path.join(os.getcwd(), 'src', 'evaluation', 'datasets', 'vww01')
-    env['CM_MLPERF_TINY_TRAINING'] = os.path.join(os.getcwd(), 'src', 'training')
-    env['CM_MLPERF_TINY_TRAINING_AD'] = os.path.join(os.getcwd(), 'src', 'training', 'anomaly_detection')
-    env['CM_MLPERF_TINY_TRAINING_IC'] = os.path.join(os.getcwd(), 'src', 'training', 'image_classification')
-    env['CM_MLPERF_TINY_TRAINING_KWS'] = os.path.join(os.getcwd(), 'src', 'training', 'keyword_spotting')
-    env['CM_MLPERF_TINY_TRAINING_VWW'] = os.path.join(os.getcwd(), 'src', 'training', 'visual_wake_words')
+    env['CM_MLPERF_TINY_DATASETS'] = os.path.join(os.getcwd(), 'src', 'benchmark', 'evaluation', 'datasets')
+    env['CM_MLPERF_TINY_DATASETS_AD'] = os.path.join(os.getcwd(), 'src', 'benchmark', 'evaluation', 'datasets', 'ad01')
+    env['CM_MLPERF_TINY_DATASETS_IC'] = os.path.join(os.getcwd(), 'src', 'benchmark', 'evaluation', 'datasets', 'ic01')
+    env['CM_MLPERF_TINY_DATASETS_KWS'] = os.path.join(os.getcwd(), 'src', 'benchmark', 'evaluation', 'datasets', 'kws01')
+    env['CM_MLPERF_TINY_DATASETS_KWS_OPEN'] = os.path.join(os.getcwd(), 'src', 'benchmark', 'evaluation', 'datasets', 'kws01-open')
+    env['CM_MLPERF_TINY_DATASETS_VWW'] = os.path.join(os.getcwd(), 'src', 'benchmark', 'evaluation', 'datasets', 'vww01')
+    env['CM_MLPERF_TINY_TRAINING'] = os.path.join(os.getcwd(), 'src', 'benchmark', 'training')
+    env['CM_MLPERF_TINY_TRAINING_AD'] = os.path.join(os.getcwd(), 'src', 'benchmark', 'training', 'anomaly_detection')
+    env['CM_MLPERF_TINY_TRAINING_IC'] = os.path.join(os.getcwd(), 'src', 'benchmark', 'training', 'image_classification')
+    env['CM_MLPERF_TINY_TRAINING_KWS'] = os.path.join(os.getcwd(), 'src', 'benchmark', 'training', 'keyword_spotting')
+    env['CM_MLPERF_TINY_TRAINING_VWW'] = os.path.join(os.getcwd(), 'src', 'benchmark', 'training', 'visual_wake_words')
 
 #        20221024: we save and restore env in the main script and can clean env here for determinism
 #    if '+PYTHONPATH' not in env: env['+PYTHONPATH'] = []
