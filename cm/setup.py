9383121bf665b723b4b9b1850de1da9d4bb315ed
Grigori Fursin <Grigori.Fursin@cTuning.org>
2023-06-06 11:34:45 +0200

    added setup.py for cmr

--- cm/setup.py
+++ cm/setup.py
@@ -98,7 +98,8 @@ setup(
 
     entry_points={"console_scripts": [
                       "cmind = cmind.cli:run",
-                      "cm = cmind.cli:run"
+                      "cm = cmind.cli:run",
+                      "cmr = cmind.cli:run_script"
                   ]},
 
     zip_safe=False,
