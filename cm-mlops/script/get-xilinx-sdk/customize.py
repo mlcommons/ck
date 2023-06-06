8f4c915b01b89c211682d025f296cdd4ab099a91
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-06-06 08:46:32 +0100

    Fix xilinx sdk detect

--- cm-mlops/script/get-xilinx-sdk/customize.py
+++ cm-mlops/script/get-xilinx-sdk/customize.py
@@ -13,10 +13,15 @@ def preprocess(i):
 
     quiet = (env.get('CM_QUIET', False) == 'yes')
 
-    file_path = env.get("CM_XILINX_SDK_FILE_PATH")
+    file_path = env.get("CM_XILINX_SDK_BIN_PATH")
     if not file_path or not os.path.exists(file_path):
         return {'return':1, 'error': 'FILE_PATH does not exist'}
 
+    bin_folder_path = os.path.dirname(file_path)
+    if '+PATH' in env:
+        env['+PATH'].append(bin_foler_path)
+    else:
+        env['+PATH'] = [ bin_folder_path ]
 
     return {'return':0}
 
