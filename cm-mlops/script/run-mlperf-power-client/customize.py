23460061aa312f82cc42839cf984a1b4aeb88a96
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-06-15 22:04:50 +0100

    Fixes for mlperf-power dummy code

--- cm-mlops/script/run-mlperf-power-client/customize.py
+++ cm-mlops/script/run-mlperf-power-client/customize.py
@@ -16,6 +16,9 @@ def preprocess(i):
     else:
         timestamp = " --no-timestamp-path"
 
+    if 'CM_MLPERF_LOADGEN_LOGS_DIR' not in env:
+        env['CM_MLPERF_LOADGEN_LOGS_DIR'] = os.path.join(os.getcwd(), "loadgen_logs")
+
     run_cmd = env['CM_MLPERF_RUN_CMD'].replace("'", '"')
     run_cmd = run_cmd.replace('"', '\\"')
     cmd = env['CM_PYTHON_BIN_WITH_PATH'] + ' ' +\
@@ -26,8 +29,10 @@ def preprocess(i):
             " -o " + env['CM_MLPERF_POWER_LOG_DIR'] + \
             " -n " + env['CM_MLPERF_POWER_NTP_SERVER'] + \
             timestamp
-           # " --max-amps " + env['CM_MLPERF_POWER_MAX_AMPS'] + \
-           # " --max-volts " + env['CM_MLPERF_POWER_MAX_VOLTS'] + \
+
+    if 'CM_MLPERF_POWER_MAX_AMPS' in env and 'CM_MLPERF_POWER_MAX_VOLTS' in env:
+           cmd = cmd + " --max-amps " + env['CM_MLPERF_POWER_MAX_AMPS'] + \
+                " --max-volts " + env['CM_MLPERF_POWER_MAX_VOLTS']
 
     env['CM_MLPERF_POWER_RUN_CMD'] = cmd
 
