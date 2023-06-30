aa9b9f8a2ce44aa034e7d59cae648f40b97b3a50
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-06-30 21:27:11 +0100

    Support COPY in dockerfile

--- cm-mlops/script/get-cuda/run.sh
+++ cm-mlops/script/get-cuda/run.sh
@@ -5,4 +5,8 @@ fi
 nvcc_bin=${CM_NVCC_BIN_WITH_PATH:-nvcc}
 
 ${nvcc_bin} -V > tmp-ver.out
+
+if [[ ${nvcc_bin} == "nvcc" ]]; then
+  nvcc_path=`which nvcc`
+  echo "CM_NVCC_BIN_WITH_PATH=${nvcc_path}" >> tmp-run-env.out
 test $? -eq 0 || exit 1
