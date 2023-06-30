075ccf01d29ce4fce07ef153c3316160b3eec523
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-06-30 16:48:37 +0100

    Support COPY in dockerfile

--- cm-mlops/script/get-cuda/run.sh
+++ cm-mlops/script/get-cuda/run.sh
@@ -2,7 +2,7 @@
 if [[ ${CM_CUDA_FULL_TOOLKIT_INSTALL} == "no" ]]; then
   exit 0
 fi
-nvcc_bin=${CM_NVCC_BIN_WITH_PATH}
+nvcc_bin=${CM_NVCC_BIN_WITH_PATH:-nvcc}
 
 ${nvcc_bin} -V > tmp-ver.out
 test $? -eq 0 || exit 1
