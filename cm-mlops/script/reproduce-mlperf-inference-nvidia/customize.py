419febf1045b040c7942f3c906303e0ae4e3ae77
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-07-04 23:41:56 +0000

    Fixed nvidia-harness deps

--- cm-mlops/script/reproduce-mlperf-inference-nvidia/customize.py
+++ cm-mlops/script/reproduce-mlperf-inference-nvidia/customize.py
@@ -25,6 +25,8 @@ def preprocess(i):
     mode = env['CM_MLPERF_LOADGEN_MODE']
     #cmds.append(f"make prebuild")
 
+    make_command = env['MLPERF_NVIDIA_RUN_COMMAND']
+
     if env['CM_MODEL'] == "resnet50":
         target_data_path = os.path.join(env['MLPERF_SCRATCH_PATH'], 'data', 'imagenet')
         if not os.path.exists(target_data_path):
@@ -116,150 +118,154 @@ def preprocess(i):
         model_name = "retinanet"
 
     #cmds.append(f"make prebuild")
-    if not env.get('CM_SKIP_MODEL_DOWNLOAD', 'no') == "yes" and not os.path.exists(model_path):
-        cmds.append(f"make download_model BENCHMARKS='{model_name}'")
+    if make_command == "download_model":
+        if not os.path.exists(model_path):
+            cmds.append(f"make download_model BENCHMARKS='{model_name}'")
+        else:
+            env['CM_MLPERF_SKIP_RUN'] = "yes"
+            return {'return':0}
 
-    if not env.get('CM_SKIP_PREPROCESS_DATASET', 'no') == "yes":
+    elif make_command == "preprocess_data":
         cmds.append(f"make preprocess_data BENCHMARKS='{model_name}'")
-    scenario=env['CM_MLPERF_LOADGEN_SCENARIO'].lower()
 
-    if env['CM_MLPERF_LOADGEN_MODE'] == "accuracy":
-        test_mode = "AccuracyOnly"
-    elif env['CM_MLPERF_LOADGEN_MODE'] == "performance":
-        test_mode = "PerformanceOnly"
-    elif env['CM_MLPERF_LOADGEN_MODE'] == "compliance":
-        test_mode = ""
-        test_name = env.get('CM_MLPERF_LOADGEN_COMPLIANCE_TEST', 'test01').lower()
-        env['CM_MLPERF_NVIDIA_RUN_COMMAND'] = "run_audit_{}_once".format(test_name)
     else:
-        return {'return': 1, 'error': 'Unsupported mode: {}'.format(env['CM_MLPERF_LOADGEN_MODE'])}
+        scenario=env['CM_MLPERF_LOADGEN_SCENARIO'].lower()
 
-    run_config = ''
+        if env['CM_MLPERF_LOADGEN_MODE'] == "accuracy":
+            test_mode = "AccuracyOnly"
+        elif env['CM_MLPERF_LOADGEN_MODE'] == "performance":
+            test_mode = "PerformanceOnly"
+        elif env['CM_MLPERF_LOADGEN_MODE'] == "compliance":
+            test_mode = ""
+            test_name = env.get('CM_MLPERF_LOADGEN_COMPLIANCE_TEST', 'test01').lower()
+            env['CM_MLPERF_NVIDIA_RUN_COMMAND'] = "run_audit_{}_once".format(test_name)
+        else:
+            return {'return': 1, 'error': 'Unsupported mode: {}'.format(env['CM_MLPERF_LOADGEN_MODE'])}
 
-    target_qps = env.get('CM_MLPERF_LOADGEN_TARGET_QPS')
-    offline_target_qps = env.get('CM_MLPERF_LOADGEN_OFFLINE_TARGET_QPS')
-    server_target_qps = env.get('CM_MLPERF_LOADGEN_SERVER_TARGET_QPS')
-    if target_qps:
-        target_qps = int(target_qps)
-        if scenario == "offline" and not offline_target_qps:
-            run_config += f" --offline_expected_qps={target_qps}"
-        elif scenario == "server" and not server_target_qps:
-            run_config += f" --server_target_qps={target_qps}"
+        run_config = ''
 
-    if offline_target_qps:
-        offline_target_qps = int(offline_target_qps)
-        run_config += f" --offline_expected_qps={offline_target_qps}"
-    if server_target_qps:
-        server_target_qps = int(server_target_qps)
-        run_config += f" --server_target_qps={server_target_qps}"
+        target_qps = env.get('CM_MLPERF_LOADGEN_TARGET_QPS')
+        offline_target_qps = env.get('CM_MLPERF_LOADGEN_OFFLINE_TARGET_QPS')
+        server_target_qps = env.get('CM_MLPERF_LOADGEN_SERVER_TARGET_QPS')
+        if target_qps:
+            target_qps = int(target_qps)
+            if scenario == "offline" and not offline_target_qps:
+                run_config += f" --offline_expected_qps={target_qps}"
+            elif scenario == "server" and not server_target_qps:
+                run_config += f" --server_target_qps={target_qps}"
 
-    target_latency = env.get('CM_MLPERF_LOADGEN_TARGET_LATENCY')
-    singlestream_target_latency = env.get('CM_MLPERF_LOADGEN_SINGLESTREAM_TARGET_LATENCY')
-    multistream_target_latency = env.get('CM_MLPERF_LOADGEN_MULTISTREAM_TARGET_LATENCY')
-    if target_latency:
-        target_latency_ns = int(float(target_latency) * 1000000)
-        if scenario == "singlestream" and not singlestream_target_latency:
-            run_config += f" --single_stream_expected_latency_ns={target_latency_ns}"
-        elif scenario == "multistream" and not multistream_target_latency:
-            run_config += f" --multi_stream_expected_latency_ns={target_latency_ns}"
+        if offline_target_qps:
+            offline_target_qps = int(offline_target_qps)
+            run_config += f" --offline_expected_qps={offline_target_qps}"
+        if server_target_qps:
+            server_target_qps = int(server_target_qps)
+            run_config += f" --server_target_qps={server_target_qps}"
 
-    if singlestream_target_latency:
-        singlestream_target_latency_ns = int(float(singlestream_target_latency) * 1000000)
-        run_config += f" --single_stream_expected_latency_ns={singlestream_target_latency_ns}"
-    if multistream_target_latency:
-        multistream_target_latency_ns = int(float(multistream_target_latency) * 1000000)
-        run_config += f" --multi_stream_expected_latency_ns={multistream_target_latency_ns}"
+        target_latency = env.get('CM_MLPERF_LOADGEN_TARGET_LATENCY')
+        singlestream_target_latency = env.get('CM_MLPERF_LOADGEN_SINGLESTREAM_TARGET_LATENCY')
+        multistream_target_latency = env.get('CM_MLPERF_LOADGEN_MULTISTREAM_TARGET_LATENCY')
+        if target_latency:
+            target_latency_ns = int(float(target_latency) * 1000000)
+            if scenario == "singlestream" and not singlestream_target_latency:
+                run_config += f" --single_stream_expected_latency_ns={target_latency_ns}"
+            elif scenario == "multistream" and not multistream_target_latency:
+                run_config += f" --multi_stream_expected_latency_ns={target_latency_ns}"
 
-    use_triton = env.get('CM_MLPERF_NVIDIA_HARNESS_USE_TRITON')
-    if use_triton:
-        run_config += f" --use_triton --config_ver=triton"
+        if singlestream_target_latency:
+            singlestream_target_latency_ns = int(float(singlestream_target_latency) * 1000000)
+            run_config += f" --single_stream_expected_latency_ns={singlestream_target_latency_ns}"
+        if multistream_target_latency:
+            multistream_target_latency_ns = int(float(multistream_target_latency) * 1000000)
+            run_config += f" --multi_stream_expected_latency_ns={multistream_target_latency_ns}"
 
-    user_conf_path = env.get('CM_MLPERF_USER_CONF')
-    if user_conf_path:
-        run_config += f" --user_conf_path={user_conf_path}"
+        use_triton = env.get('CM_MLPERF_NVIDIA_HARNESS_USE_TRITON')
+        if use_triton:
+            run_config += f" --use_triton --config_ver=triton"
 
-    mlperf_conf_path = env.get('CM_MLPERF_INFERENCE_CONF_PATH')
-    if mlperf_conf_path:
-        run_config += f" --mlperf_conf_path={mlperf_conf_path}"
+        user_conf_path = env.get('CM_MLPERF_USER_CONF')
+        if user_conf_path:
+            run_config += f" --user_conf_path={user_conf_path}"
 
-    power_setting = env.get('CM_MLPERF_NVIDIA_HARNESS_POWER_SETTING')
-    if power_setting:
-        run_config += f" --power_setting={power_setting}"
+        mlperf_conf_path = env.get('CM_MLPERF_INFERENCE_CONF_PATH')
+        if mlperf_conf_path:
+            run_config += f" --mlperf_conf_path={mlperf_conf_path}"
 
-    gpu_copy_streams = env.get('CM_MLPERF_NVIDIA_HARNESS_GPU_COPY_STREAMS')
-    if gpu_copy_streams:
-        run_config += f" --gpu_copy_streams={gpu_copy_streams}"
-    gpu_inference_streams = env.get('CM_MLPERF_NVIDIA_HARNESS_GPU_INFERENCE_STREAMS')
-    if gpu_inference_streams:
-        run_config += f" --gpu_inference_streams={gpu_inference_streams}"
-    dla_copy_streams = env.get('CM_MLPERF_NVIDIA_HARNESS_DLA_COPY_STREAMS')
-    if dla_copy_streams:
-        run_config += f" --dla_copy_streams={dla_copy_streams}"
-    dla_inference_streams = env.get('CM_MLPERF_NVIDIA_HARNESS_DLA_INFERENCE_STREAMS')
-    if dla_inference_streams:
-        run_config += f" --dla_inference_streams={dla_inference_streams}"
+        power_setting = env.get('CM_MLPERF_NVIDIA_HARNESS_POWER_SETTING')
+        if power_setting:
+            run_config += f" --power_setting={power_setting}"
 
-    gpu_batch_size = env.get('CM_MLPERF_NVIDIA_HARNESS_GPU_BATCH_SIZE')
-    if gpu_batch_size:
-        run_config += f" --gpu_batch_size={gpu_batch_size}"
-    dla_batch_size = env.get('CM_MLPERF_NVIDIA_HARNESS_DLA_BATCH_SIZE')
-    if dla_batch_size:
-        run_config += f" --dla_batch_size={dla_batch_size}"
+        gpu_copy_streams = env.get('CM_MLPERF_NVIDIA_HARNESS_GPU_COPY_STREAMS')
+        if gpu_copy_streams:
+            run_config += f" --gpu_copy_streams={gpu_copy_streams}"
+        gpu_inference_streams = env.get('CM_MLPERF_NVIDIA_HARNESS_GPU_INFERENCE_STREAMS')
+        if gpu_inference_streams:
+            run_config += f" --gpu_inference_streams={gpu_inference_streams}"
+        dla_copy_streams = env.get('CM_MLPERF_NVIDIA_HARNESS_DLA_COPY_STREAMS')
+        if dla_copy_streams:
+            run_config += f" --dla_copy_streams={dla_copy_streams}"
+        dla_inference_streams = env.get('CM_MLPERF_NVIDIA_HARNESS_DLA_INFERENCE_STREAMS')
+        if dla_inference_streams:
+            run_config += f" --dla_inference_streams={dla_inference_streams}"
 
-    input_format = env.get('CM_MLPERF_NVIDIA_HARNESS_INPUT_FORMAT')
-    if input_format:
-        run_config += f" --input_format={input_format}"
+        gpu_batch_size = env.get('CM_MLPERF_NVIDIA_HARNESS_GPU_BATCH_SIZE')
+        if gpu_batch_size:
+            run_config += f" --gpu_batch_size={gpu_batch_size}"
+        dla_batch_size = env.get('CM_MLPERF_NVIDIA_HARNESS_DLA_BATCH_SIZE')
+        if dla_batch_size:
+            run_config += f" --dla_batch_size={dla_batch_size}"
 
-    performance_sample_count = env.get('CM_MLPERF_LOADGEN_PERFORMANCE_SAMPLE_COUNT')
-    if performance_sample_count:
-        run_config += f" --performance_sample_count={performance_sample_count}"
+        input_format = env.get('CM_MLPERF_NVIDIA_HARNESS_INPUT_FORMAT')
+        if input_format:
+            run_config += f" --input_format={input_format}"
 
-    devices = env.get('CM_MLPERF_NVIDIA_HARNESS_DEVICES')
-    if devices:
-        run_config += f" --devices={devices}"
+        performance_sample_count = env.get('CM_MLPERF_LOADGEN_PERFORMANCE_SAMPLE_COUNT')
+        if performance_sample_count:
+            run_config += f" --performance_sample_count={performance_sample_count}"
+
+        devices = env.get('CM_MLPERF_NVIDIA_HARNESS_DEVICES')
+        if devices:
+            run_config += f" --devices={devices}"
 
 
-    workspace_size = env.get('CM_MLPERF_NVIDIA_HARNESS_WORKSPACE_SIZE')
-    if workspace_size:
-        run_config += f" --workspace_size={workspace_size}"
+        workspace_size = env.get('CM_MLPERF_NVIDIA_HARNESS_WORKSPACE_SIZE')
+        if workspace_size:
+            run_config += f" --workspace_size={workspace_size}"
 
-    if env.get('CM_MLPERF_LOADGEN_LOGS_DIR'):
-        env['MLPERF_LOADGEN_LOGS_DIR'] = env['CM_MLPERF_LOADGEN_LOGS_DIR']
+        if env.get('CM_MLPERF_LOADGEN_LOGS_DIR'):
+            env['MLPERF_LOADGEN_LOGS_DIR'] = env['CM_MLPERF_LOADGEN_LOGS_DIR']
 
-    log_dir = env.get('CM_MLPERF_NVIDIA_HARNESS_LOG_DIR')
-    if log_dir:
-        run_config += f" --log_dir={log_dir}"
+        log_dir = env.get('CM_MLPERF_NVIDIA_HARNESS_LOG_DIR')
+        if log_dir:
+            run_config += f" --log_dir={log_dir}"
 
-    use_graphs = env.get('CM_MLPERF_NVIDIA_HARNESS_USE_GRAPHS')
-    if use_graphs:
-        run_config += " --use_graphs"
+        use_graphs = env.get('CM_MLPERF_NVIDIA_HARNESS_USE_GRAPHS')
+        if use_graphs:
+            run_config += " --use_graphs"
 
-    run_infer_on_copy_streams = env.get('CM_MLPERF_NVIDIA_HARNESS_RUN_INFER_ON_COPY_STREAMS')
-    if run_infer_on_copy_streams:
-        run_config += " --run_infer_on_copy_streams"
+        run_infer_on_copy_streams = env.get('CM_MLPERF_NVIDIA_HARNESS_RUN_INFER_ON_COPY_STREAMS')
+        if run_infer_on_copy_streams:
+            run_config += " --run_infer_on_copy_streams"
 
-    start_from_device = env.get('CM_MLPERF_NVIDIA_HARNESS_START_FROM_DEVICE')
-    if start_from_device:
-        run_config += " --start_from_device"
+        start_from_device = env.get('CM_MLPERF_NVIDIA_HARNESS_START_FROM_DEVICE')
+        if start_from_device:
+            run_config += " --start_from_device"
 
-    end_on_device = env.get('CM_MLPERF_NVIDIA_HARNESS_END_ON_DEVICE')
-    if end_on_device:
-        run_config += " --end_on_device"
+        end_on_device = env.get('CM_MLPERF_NVIDIA_HARNESS_END_ON_DEVICE')
+        if end_on_device:
+            run_config += " --end_on_device"
 
-    max_dlas = env.get('CM_MLPERF_NVIDIA_HARNESS_MAX_DLAS')
-    if max_dlas:
-        run_config += f" --max_dlas={max_dlas}"
+        max_dlas = env.get('CM_MLPERF_NVIDIA_HARNESS_MAX_DLAS')
+        if max_dlas:
+            run_config += f" --max_dlas={max_dlas}"
 
-    make_command = env.get('CM_MLPERF_NVIDIA_RUN_COMMAND', 'run')
+        if test_mode:
+            test_mode_string = " --test_mode={}".format(test_mode)
+        else:
+            test_mode_string = ""
 
-    if test_mode:
-        test_mode_string = " --test_mode={}".format(test_mode)
-    else:
-        test_mode_string = ""
+        run_config += " --no_audit_verify"
 
-    run_config += " --no_audit_verify"
-
-    cmds.append(f"make {make_command} RUN_ARGS=' --benchmarks={model_name} --scenarios={scenario} {test_mode_string} {run_config}'")
+        cmds.append(f"make {make_command} RUN_ARGS=' --benchmarks={model_name} --scenarios={scenario} {test_mode_string} {run_config}'")
     #print(cmds)
     run_cmd = " && ".join(cmds)
     env['CM_MLPERF_RUN_CMD'] = run_cmd
