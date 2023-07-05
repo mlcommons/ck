ef91e1ca03205d9100a6161cd6ca9c5674b47e6f
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-07-05 10:50:13 +0100

    Added option to install cuda driver

--- cm-mlops/script/install-cuda-prebuilt/run.sh
+++ cm-mlops/script/install-cuda-prebuilt/run.sh
@@ -4,5 +4,5 @@ CUR=${PWD}
 
 INSTALL_DIR=${CUR}/install
 
-${CM_SUDO} bash ${CM_CUDA_RUN_FILE_PATH} --toolkitpath=${INSTALL_DIR} --defaultroot=${INSTALL_DIR} --toolkit --silent
+${CM_SUDO} bash ${CM_CUDA_RUN_FILE_PATH} --toolkitpath=${INSTALL_DIR} --defaultroot=${INSTALL_DIR} --toolkit ${CUDA_ADDITIONAL_INSTALL_OPTIONS} --silent
 
