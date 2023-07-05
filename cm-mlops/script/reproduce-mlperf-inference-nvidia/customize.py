6a89985352239b61664938ad4db2242a35297aef
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-07-05 13:54:46 +0100

    Use nvidia script for retinanet download

--- cm-mlops/script/reproduce-mlperf-inference-nvidia/customize.py
+++ cm-mlops/script/reproduce-mlperf-inference-nvidia/customize.py
@@ -107,14 +107,10 @@ def preprocess(i):
             cmds.append(f"ln -s {calibration_dataset_path} {target_data_path}")
 
         preprocessed_data_path = os.path.join(env['MLPERF_SCRATCH_PATH'], 'preprocessed_data')
-        model_path = env.get('CM_NVIDIA_RETINANET_EFFICIENT_NMS_CONCAT_MODEL_WITH_PATH','')
         target_model_path_dir = os.path.join(env['MLPERF_SCRATCH_PATH'], 'models', 'retinanet-resnext50-32x4d', 'submission')
-        '''if not os.path.exists(target_model_path_dir):
+        if not os.path.exists(target_model_path_dir):
             cmds.append(f"mkdir -p {target_model_path_dir}")
-        target_model_path = os.path.join(target_model_path_dir, 'retinanet_resnext50_32x4d_efficientNMS.800x800.onnx')
-        if not os.path.exists(target_model_path):
-            cmds.append(f"ln -sf {model_path} {target_model_path}")
-        '''
+        model_path = os.path.join(target_model_path_dir, 'retinanet_resnext50_32x4d_efficientNMS.800x800.onnx')
         model_name = "retinanet"
 
     #cmds.append(f"make prebuild")
