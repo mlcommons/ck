2e7378591517c6e7ba6e47493b744d46977b73ca
Arjun <arjunsuresh1987@gmail.com>
2023-07-09 03:32:14 +0100

    Fixes to mlperf power

--- cm-mlops/script/benchmark-program-mlperf/customize.py
+++ cm-mlops/script/benchmark-program-mlperf/customize.py
@@ -4,17 +4,18 @@ import os
 def preprocess(i):
     os_info = i['os_info']
     env = i['env']
-    if env.get('CM_MLPERF_POWER', '') == "yes":
-        if env.get('CM_MLPERF_SHORT_RANGING_RUN', '') == 'yes':
-            os.system("echo '0' > count.txt")
-            env['CM_MLPERF_RUN_CMD'] = "CM_MLPERF_RUN_COUNT=\$(cat count.txt); echo \${CM_MLPERF_RUN_COUNT};  CM_MLPERF_RUN_COUNT=\$((CM_MLPERF_RUN_COUNT+1));   echo \${CM_MLPERF_RUN_COUNT} > count.txt && if [ \${CM_MLPERF_RUN_COUNT} -eq \'1\' ]; then export CM_MLPERF_USER_CONF_PREFIX=\'ranging_\'; else export CM_MLPERF_USER_CONF_PREFIX=\'\'; fi && "+env['CM_RUN_CMD'].strip()
-        else:
-            env['CM_MLPERF_RUN_CMD'] = env['CM_RUN_CMD'].strip()
 
     return {'return':0}
 
 def postprocess(i):
 
     env = i['env']
+    env['CM_MLPERF_RUN_CMD'] = env.get('CM_RUN_CMD')
+    if env.get('CM_MLPERF_POWER', '') == "yes":
+        if env.get('CM_MLPERF_SHORT_RANGING_RUN', '') == 'yes':
+            os.system("echo '0' > "+env.get('CM_RUN_DIR','')+ "/count.txt")
+            env['CM_MLPERF_RUN_CMD'] = "CM_MLPERF_RUN_COUNT=\$(cat \${CM_RUN_DIR}/count.txt); echo \${CM_MLPERF_RUN_COUNT};  CM_MLPERF_RUN_COUNT=\$((CM_MLPERF_RUN_COUNT+1));   echo \${CM_MLPERF_RUN_COUNT} > \${CM_RUN_DIR}/count.txt && if [ \${CM_MLPERF_RUN_COUNT} -eq \'1\' ]; then export CM_MLPERF_USER_CONF=\${CM_MLPERF_RANGING_USER_CONF}; else export CM_MLPERF_USER_CONF=\${CM_MLPERF_TESTING_USER_CONF}; fi && "+env.get('CM_RUN_CMD','').strip()
+        else:
+            env['CM_MLPERF_RUN_CMD'] = env.get('CM_RUN_CMD','').strip()
 
     return {'return':0}
