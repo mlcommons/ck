32c156e63f33df5eec3c3724d5602c8f59d1720f
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-07-12 16:19:49 +0100

    Fix bert model meta

--- cm-mlops/script/get-ml-model-bert-large-squad/customize.py
+++ cm-mlops/script/get-ml-model-bert-large-squad/customize.py
@@ -15,9 +15,9 @@ def postprocess(i):
     env['CM_ML_MODEL_FILE'] = os.path.basename(env['CM_ML_MODEL_FILE_WITH_PATH'])
 
     if env.get('CM_ML_MODEL_PRECISION', '') == "fp32":
-        env['CM_ML_MODEL_BERT_LARGE_FP32_PATH'] = os.path.basename(env['CM_ML_MODEL_FILE_WITH_PATH'])
+        env['CM_ML_MODEL_BERT_LARGE_FP32_PATH'] = env['CM_ML_MODEL_FILE_WITH_PATH']
     elif env.get('CM_ML_MODEL_PRECISION', '') == "int8":
-        env['CM_ML_MODEL_BERT_LARGE_INT8_PATH'] = os.path.basename(env['CM_ML_MODEL_FILE_WITH_PATH'])
+        env['CM_ML_MODEL_BERT_LARGE_INT8_PATH'] = env['CM_ML_MODEL_FILE_WITH_PATH']
 
     env['CM_GET_DEPENDENT_CACHED_PATH'] = env['CM_ML_MODEL_FILE_WITH_PATH']
 
