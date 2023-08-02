c99186ca06bbb9f3f26c698cc771bd47f116d7ae
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2023-08-03 02:23:19 +0530

    added support for bert pruning (#1)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-89-56.ec2.internal>

--- cm-mlops/script/prune-bert-models/customize.py
+++ cm-mlops/script/prune-bert-models/customize.py
@@ -6,9 +6,12 @@ def preprocess(i):
     os_info = i['os_info']
 
     env = i['env']
-
-    print("Entered preprocess")
-
+    env['BERT_PRUNE_REPO_PATH'] = env['CM_GIT_CHECKOUT_PATH']
+    print("Pruning repo path:"+env['BERT_PRUNE_REPO_PATH'])
+    env['CM_UNPRUNED_MODEL_PATH']=env['CM_ML_MODEL_FILE_WITH_PATH']+"models--bert-large-uncased/snapshots/80792f8e8216b29f3c846b653a0ff0a37c210431"
+    out_dir="/home/ubuntu/prune_model/out"
+    cmd = "python3 "+env['BERT_PRUNE_REPO_PATH']+"/main.py --model_name " + env['CM_PRUNE_MODEL_NAME'] + " --task_name " + env['CM_PRUNE_TASK'] +  " --ckpt_dir "+env['CM_UNPRUNED_MODEL_PATH']+" --constraint 0.5 --output_dir "+out_dir
+    os.system(cmd)
     return {'return': 0}
 
 def postprocess(i):
