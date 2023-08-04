d8ce5228c2adbb16090be14898d959631bfb6f01
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-08-04 22:16:55 +0100

    improve the user experience with compliance runs

--- cm-mlops/script/generate-mlperf-inference-user-conf/customize.py
+++ cm-mlops/script/generate-mlperf-inference-user-conf/customize.py
@@ -103,13 +103,21 @@ def preprocess(i):
         metric = "target_qps"
         tolerance = 1.01
         #value = env.get('CM_MLPERF_LOADGEN_SERVER_TARGET_QPS') if scenario == "Server" else env.get('CM_MLPERF_LOADGEN_OFFLINE_TARGET_QPS')
-        if not value:
-            value = env.get('CM_MLPERF_LOADGEN_TARGET_QPS')
+        value = env.get('CM_MLPERF_LOADGEN_TARGET_QPS')
+        if mode == "compliance" and scenario == "Server": #Adjust the server_target_qps
+            test = env.get("CM_MLPERF_LOADGEN_COMPLIANCE_TEST", "TEST01")
+            if test == "TEST01":
+                value = str(int(value) * int(env.get("CM_MLPERF_TEST01_SERVER_ADJUST_FACTOR", 0.96)))
+            if test == "TEST05":
+                value = str(int(value) * int(env.get("CM_MLPERF_TEST05_SERVER_ADJUST_FACTOR", 0.97)))
     elif scenario in [ 'SingleStream', 'MultiStream' ]:
         metric = "target_latency"
         tolerance = 0.4 #much lower because we have max_duration
-        if not value:
-            value = env.get('CM_MLPERF_LOADGEN_TARGET_LATENCY')
+        value = env.get('CM_MLPERF_LOADGEN_TARGET_LATENCY')
+        if scenario == "SingleStream" and (1000/float(value) * 660 < 100):
+            env['CM_MLPERF_USE_MAX_DURATION'] = 'no'
+        elif scenario == "MultiStream" and (1000/float(value) * 660 < 662):
+            env['CM_MLPERF_USE_MAX_DURATION'] = 'no'
     else:
         return {'return': 1, 'error': 'Invalid scenario: {}'.format(scenario)}
 
