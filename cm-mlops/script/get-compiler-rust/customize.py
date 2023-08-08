8452fee134a2ee78ac6b0c7429a44d4b8bf94dd0
Arjun <arjunsuresh1987@gmail.com>
2023-08-08 14:26:55 +0100

    Added safetensor dependency for nvidia bert

---
+++ cm-mlops/script/get-compiler-rust/customize.py
@@ -0,0 +1,24 @@
+from cmind import utils
+import os
+
+def preprocess(i):
+
+    os_info = i['os_info']
+
+    env = i['env']
+
+    meta = i['meta']
+
+    automation = i['automation']
+
+    quiet = (env.get('CM_QUIET', False) == 'yes')
+
+    return {'return':0}
+
+def postprocess(i):
+
+    env = i['env']
+
+    env['+PATH'] = [ os.path.join(os.path.expanduser('~'),".cargo", "bin") ]
+
+    return {'return':0}
--- cm-mlops/script/get-compiler-rust/run.sh
+++ cm-mlops/script/get-compiler-rust/run.sh
@@ -5,9 +5,3 @@ ${CM_PYTHON_BIN} -m pip install setuptools testresources wheel h5py --user --upg
 
 curl https://sh.rustup.rs -sSf -o tmp.sh
 sh tmp.sh -y 
-
-export PATH=$PATH:$HOME/.cargo/bin
-
-${CM_PYTHON_BIN} -m pip install tensorflow-aarch64${CM_TMP_PIP_VERSION_STRING}  --user ${CM_PYTHON_PIP_COMMON_EXTRA}
-test $? -eq 0 || exit 1
-echo "CM_GENERIC_PYTHON_PACKAGE_NAME=tensorflow-aarch64" >> $PWD/tmp-run-env.out
