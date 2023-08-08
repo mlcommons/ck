805a71782529040920398640484a4d2830ca67fd
Grigori Fursin <Grigori.Fursin@cTuning.org>
2023-08-08 12:51:06 +0200

    minor clean up

--- cm-mlops/script/build-docker-image/examples/0-common.bat
+++ cm-mlops/script/build-docker-image/examples/0-common.bat
@@ -6,9 +6,9 @@ rem set DOCKER_OS_VER=22.04
 set DOCKER_OS_VER=23.04
 set DOCKER_PIP_EXTRA_FLAGS=--break-system-packages
 
-set DOCKER_IMAGE_NAME=cm-base
-rem set DOCKER_IMAGE_NAME=cm-script-app-image-classification-onnx-py
-rem set DOCKER_IMAGE_POST_FILE=%CD%\extra-cmd.cm-script-app-image-classification-onnx-py
+rem set DOCKER_IMAGE_NAME=cm-base
+set DOCKER_IMAGE_NAME=cm-script-app-image-classification-onnx-py
+set DOCKER_IMAGE_POST_FILE=%CD%\extra-cmd.cm-script-app-image-classification-onnx-py
 
 rem set DOCKER_IMAGE_TAG=%DOCKER_OS%-%DOCKER_OS_VER%-20230804
 
--- cm-mlops/script/build-docker-image/examples/run-cm-image-classification-python-onnx-with-file.bat
+++ cm-mlops/script/build-docker-image/examples/run-cm-image-classification-python-onnx-with-file.bat
@@ -2,4 +2,5 @@ rem call this script with computer_mouse.jpg as input
 
 call 0-common.bat
 
-docker run -v %CD%:/tmp/host -it --rm cknowledge/cm-script-app-image-classification-onnx-py:ubuntu-23.04-latest -c "time cmr 'python app image-classification onnx' --adr.python.name=cm --input=/tmp/host/%1"
+rem docker run -v %CD%:/tmp/host -it --rm cknowledge/cm-script-app-image-classification-onnx-py:ubuntu-23.04-latest -c "time cmr 'python app image-classification onnx' --adr.python.name=cm --input=/tmp/host/%1"
+docker run -v %CD%:/tmp/host -it --rm %DOCKER_IMAGE_REPO%/%DOCKER_IMAGE_NAME%:%DOCKER_IMAGE_TAG% -c "time cmr 'python app image-classification onnx' --adr.python.name=cm --input=/tmp/host/%1"
