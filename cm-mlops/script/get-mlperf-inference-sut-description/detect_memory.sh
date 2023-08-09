2eb67e358d6870ba6e801b31912b85dd8a490595
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-08-10 00:00:36 +0100

    Fix for dmidecode

--- cm-mlops/script/get-mlperf-inference-sut-description/detect_memory.sh
+++ cm-mlops/script/get-mlperf-inference-sut-description/detect_memory.sh
@@ -2,4 +2,6 @@
 
 if [[ ${CM_SUDO_USER} == "yes" ]]; then
   sudo dmidecode -t memory > meminfo.out
+  ${CM_PYTHON_BIN_WITH_PATH} ${CM_TMP_CURRENT_SCRIPT_PATH}/get_memory_info.py
 fi
+test $? -eq 0 || return $?
