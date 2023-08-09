b8fccbe864d21c82a9b5f1398231fdaa9663bb7e
Arjun <arjunsuresh1987@gmail.com>
2023-08-09 23:06:02 +0100

    minor cleanup for mobilenet run script

--- cm-mlops/script/run-all-mlperf-models/run-mobilenet-models.sh
+++ cm-mlops/script/run-all-mlperf-models/run-mobilenet-models.sh
@@ -23,32 +23,41 @@ function run() {
   fi
 }
 POWER=" --power=yes --adr.mlperf-power-client.power_server=192.168.0.15 --adr.mlperf-power-client.port=4940 "
+extra_option=" --adr.mlperf-inference-implementation.compressed_dataset=on"
+
+
 #Add your run commands here...
 # run "$CM_RUN_CMD"
 run "cm run script --tags=run,mobilenet-models,_tflite,_accuracy-only \
 --adr.compiler.tags=gcc \
+${extra_option} \
 --results_dir=$HOME/mobilenet_results"
 
 run "cm run script --tags=run,mobilenet-models,_tflite,_performance-only \
 ${POWER} \
 --adr.compiler.tags=gcc \
+${extra_option} \
 --results_dir=$HOME/mobilenet_results"
 
 run "cm run script --tags=run,mobilenet-models,_tflite,_populate-readme \
 ${POWER} \
 --adr.compiler.tags=gcc \
+${extra_option} \
 --results_dir=$HOME/mobilenet_results"
 
 run "cm run script --tags=run,mobilenet-models,_tflite,_armnn,_neon,_accuracy-only \
 --adr.compiler.tags=gcc \
+${extra_option} \
 --results_dir=$HOME/mobilenet_results"
 
 run "cm run script --tags=run,mobilenet-models,_tflite,_armnn,_neon,_performance-only \
 ${POWER} \
+${extra_option} \
 --adr.compiler.tags=gcc \
 --results_dir=$HOME/mobilenet_results"
 
 run "cm run script --tags=run,mobilenet-models,_tflite,_armnn,_neon,_populate-readme \
 ${POWER} \
+${extra_option} \
 --adr.compiler.tags=gcc \
 --results_dir=$HOME/mobilenet_results"
