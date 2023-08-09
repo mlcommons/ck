812ae9a6ea90cd8f3630a90e95245b7218a70c94
Arjun <arjunsuresh1987@gmail.com>
2023-08-09 23:33:08 +0000

    Cleanup of live-processing

--- cm-mlops/script/app-mlperf-inference-tflite-cpp/customize.py
+++ cm-mlops/script/app-mlperf-inference-tflite-cpp/customize.py
@@ -82,7 +82,7 @@ def preprocess(i):
     if 'CM_MLPERF_USER_CONF' not in env:
         env['CM_MLPERF_USER_CONF'] = os.path.join(env['CM_MLPERF_INFERENCE_CLASSIFICATION_AND_DETECTION_PATH'], "user.conf")
 
-    if env.get('CM_DATASET_COMPRESSED', "no").lower() in [ "yes", "on", "true"]:
+    if env.get('CM_DATASET_COMPRESSED', "no").lower() in [ "yes", "on", "true"] and "float" in env.get('CM_MLPERF_MODEL_PRECISION', ''):
         env['CM_HOST_USE_ALL_CORES'] = "yes" #Use all cores for input preprocessing
         env['CM_MLPERF_SUT_NAME_RUN_CONFIG_SUFFIX2'] = "with_live_preprocessing"
 
