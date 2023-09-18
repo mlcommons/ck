64e70594f936722844fa9eaa64229fead2cd3cf1
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-09-18 10:40:37 +0100

    Remove outdated criteo preprocessing steps

--- cm-mlops/script/get-preprocessed-dataset-criteo/run.sh
+++ cm-mlops/script/get-preprocessed-dataset-criteo/run.sh
@@ -5,4 +5,4 @@ CUR=$PWD
 if [[ ${CM_CRITEO_FAKE} == "yes" ]]; then
   exit 0
 fi
-${CM_PYTHON_BIN_WITH_PATH} ${CM_TMP_CURRENT_SCRIPT_PATH}/preprocess.py
+#${CM_PYTHON_BIN_WITH_PATH} ${CM_TMP_CURRENT_SCRIPT_PATH}/preprocess.py
