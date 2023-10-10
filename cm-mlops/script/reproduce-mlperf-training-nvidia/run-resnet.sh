3604b402b7b6c4e22f1951d74c3132e693f74b48
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-10-10 14:58:46 +0100

    Fixes for resnet training

--- cm-mlops/script/reproduce-mlperf-training-nvidia/run-resnet.sh
+++ cm-mlops/script/reproduce-mlperf-training-nvidia/run-resnet.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 benchmark_implementation=${benchmark_implementation:-"mxnet-22.04"}
 cd ${CM_MLPERF_TRAINING_NVIDIA_CODE_PATH}/benchmarks/resnet/implementations/${benchmark_implementation}
-
 docker build --pull -t mlperf-nvidia:image_classification .
 test $? -eq 0 || exit $?
 source ${CONFIG_FILE}
