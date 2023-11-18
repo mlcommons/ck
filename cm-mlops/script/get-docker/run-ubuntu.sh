4477672320fa63cae4388b58b26b4a2443a31e8f
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-11-18 16:27:17 +0530

    Fixes for get-docker

--- cm-mlops/script/get-docker/run-ubuntu.sh
+++ cm-mlops/script/get-docker/run-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo DEBIAN_FRONTEND=noninteractive apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -10,8 +10,8 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo DEBIAN_FRONTEND=noninteractive apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker $USER
 #exec newgrp docker
-sudo su - $USER
+#sudo su - $USER
 
