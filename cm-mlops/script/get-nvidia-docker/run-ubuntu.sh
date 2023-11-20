42d16c2ae7420665b89a52bc145bd5f0a278c05b
Arjun <arjunsuresh1987@gmail.com>
2023-11-20 10:51:05 +0000

    Fixes for get-docker

--- cm-mlops/script/get-nvidia-docker/run-ubuntu.sh
+++ cm-mlops/script/get-nvidia-docker/run-ubuntu.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+if [[ ! -f /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg ]]; then
+  cmd="curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg"
+  echo "$cmd"
+  eval "$cmd"
+fi
+
+cmd="curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list \
   && \
-    sudo apt-get update
+    sudo apt-get update"
+echo "$cmd"
+eval "$cmd"
+test $? -eq 0 || exit $?
 
-sudo apt-get install -y nvidia-container-toolkit
+cmd="sudo apt-get install -y nvidia-container-toolkit"
+echo "$cmd"
+eval "$cmd"
+test $? -eq 0 || exit $?
 
-sudo nvidia-ctk runtime configure --runtime=docker
+cmd="sudo nvidia-ctk runtime configure --runtime=docker"
+echo "$cmd"
+eval "$cmd"
+test $? -eq 0 || exit $?
 
-sudo systemctl restart docker
+cmd="sudo systemctl restart docker"
+cmd="sudo service docker restart"
+echo "$cmd"
+eval "$cmd"
+test $? -eq 0 || exit $?
