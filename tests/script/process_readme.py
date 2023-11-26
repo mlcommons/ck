acebc468beaa4848ba42a6c68265f2be7276ac41
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-11-26 18:00:52 +0000

    Fix bug in the tests

--- tests/script/process_readme.py
+++ tests/script/process_readme.py
@@ -8,17 +8,17 @@ import yaml
 files=sys.argv[1:]
 
 for file in files:
-  if not file.endswith("_cm.json") or not file.endswith("_cm.yaml"):
-    continue
-  if not file.startswith(os.path.join("cm-mlops", "script")):
-    continue
-  script_path = os.path.dirname(file)
-  f = open(file)
-  if f.endswith(".json")
-    data = json.load(f)
-  elif f.endswith(".yaml")
-    data = yaml.safe_load(f)
-  uid = data['uid']
+    if not file.endswith("_cm.json") or not file.endswith("_cm.yaml"):
+        continue
+    if not file.startswith(os.path.join("cm-mlops", "script")):
+        continue
+    script_path = os.path.dirname(file)
+    f = open(file)
+    if f.endswith(".json"):
+        data = json.load(f)
+    elif f.endswith(".yaml"):
+        data = yaml.safe_load(f)
+    uid = data['uid']
 
-  r = cm.access({'action':'doc', 'automation':'script', 'artifact': uid, 'quiet': 'yes'})
-  checks.check_return(r)
+    r = cm.access({'action':'doc', 'automation':'script', 'artifact': uid, 'quiet': 'yes'})
+    checks.check_return(r)
