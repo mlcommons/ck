f8302d52523e1d306847cce4ab063e3ff8977fee
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-11-30 04:14:37 +0530

    Fix bug with v2.1 code for mlperf-inference

--- tests/script/test_deps.py
+++ tests/script/test_deps.py
@@ -4,18 +4,18 @@ import cmind as cm
 import check as checks
 
 r = cm.access({'action':'run', 'automation':'script', 'tags': 'generate-run-cmds,mlperf', 'adr':
-    {'loadgen': {'version': 'r2.1'}, 'compiler': {'tags': "gcc"}}, 'env': {'CM_MODEL': 'resnet50',
+    {'loadgen': {'version': 'r3.0'}, 'compiler': {'tags': "gcc"}}, 'env': {'CM_MODEL': 'resnet50',
         'CM_DEVICE': 'cpu', 'CM_BACKEND': 'onnxruntime'}, 'quiet': 'yes'})
 checks.check_return(r)
 
-r = cm.access({'action':'search', 'automation': 'cache', 'tags': 'loadgen,version-r2.1,deps-python-non-virtual'})
-checks.check_list(r, "loadgen,version-r2.1,deps-python-non-virtual")
+r = cm.access({'action':'search', 'automation': 'cache', 'tags': 'loadgen,version-r3.0,deps-python-non-virtual'})
+checks.check_list(r, "loadgen,version-r3.0,deps-python-non-virtual")
 
-r = cm.access({'action':'search', 'automation': 'cache', 'tags': 'inference,src,version-r2.1'})
-checks.check_list(r, "inference,src,version-r2.1")
+r = cm.access({'action':'search', 'automation': 'cache', 'tags': 'inference,src,version-master'})
+checks.check_list(r, "inference,src,version-master")
 
-r = cm.access({'action':'run', 'automation':'script', 'tags': 'app,mlperf,inference,generic,_python,_resnet50,_onnxruntime,_cpu,_r2.1_default', 'adr': {'mlperf-implementation': { 'version': 'master'}}, 'quiet': 'yes'})
+r = cm.access({'action':'run', 'automation':'script', 'tags': 'app,mlperf,inference,generic,_python,_resnet50,_onnxruntime,_cpu,_r3.0_default', 'adr': {'mlperf-implementation': { 'version': 'master'}}, 'quiet': 'yes'})
 checks.check_return(r)
 
-r = cm.access({'action':'run', 'automation':'script', 'tags': 'app,mlperf,inference,generic,_python,_resnet50,_tf,_cpu,_r2.1_default', 'adr': {'mlperf-implementation': { 'version': 'master'}}, 'quiet': 'yes'})
+r = cm.access({'action':'run', 'automation':'script', 'tags': 'app,mlperf,inference,generic,_python,_resnet50,_tf,_cpu,_r3.0_default', 'adr': {'mlperf-implementation': { 'version': 'master'}}, 'quiet': 'yes'})
 checks.check_return(r)
