7f8c80a1a11c8460a14fd2b25008d8ef44fc6531
Grigori Fursin <gfursin@gmail.com>
2023-11-29 12:57:54 +0100

    fixed ABTF SSD PyTorch for Linux

--- cm-mlops/script/test-abtf-ssd-pytorch/run.sh
+++ cm-mlops/script/test-abtf-ssd-pytorch/run.sh
@@ -8,3 +8,19 @@ echo "Env CM_TMP_CURRENT_SCRIPT_PATH: ${CM_TMP_CURRENT_SCRIPT_PATH}"
 echo "Env CM_ABTF_SSD_PYTORCH: ${CM_ABTF_SSD_PYTORCH}"
 echo "Env CM_ML_MODEL_FILE_WITH_PATH: ${CM_ML_MODEL_FILE_WITH_PATH}"
 
+# Patch model
+cd ${CM_ABTF_SSD_PYTORCH}
+if [ ! -f "patchfile-20231129.patch" ] ; then
+  echo ""
+  echo "Patching ABTF SRC"
+  echo ""
+
+  cp ${CM_TMP_CURRENT_SCRIPT_PATH}/patches/patchfile-20231129.patch .
+  patch -s -p0 < patchfile-20231129.patch
+  test $? -eq 0 || exit $?
+fi
+cd ${CUR_DIR}
+
+echo ""
+${CM_PYTHON_BIN_WITH_PATH} ${CM_ABTF_SSD_PYTORCH}/test_image.py --pretrained-model ${CM_ML_MODEL_FILE_WITH_PATH} --input ${CM_INPUT_IMAGE} --output ${CM_OUTPUT_IMAGE}
+test $? -eq 0 || exit $?
