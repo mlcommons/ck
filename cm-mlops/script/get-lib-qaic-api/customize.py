1085adeb35b769fa78d1b60cf15eb8737256b42b
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-12-02 02:12:44 +0530

    Fixes for lib-qaic

--- cm-mlops/script/get-lib-qaic-api/customize.py
+++ cm-mlops/script/get-lib-qaic-api/customize.py
@@ -8,7 +8,7 @@ def preprocess(i):
 
     env = i['env']
 
-    env['CM_GIT_CHECKOUT'] = env['CM_TMP_GIT_BRANCH_NAME']
+    #env['CM_GIT_CHECKOUT'] = env['CM_TMP_GIT_BRANCH_NAME']
 
     return {'return':0}
 
@@ -26,14 +26,14 @@ def postprocess(i):
     for key in paths:
         env[key] = []
 
-    include_paths = []
+    include_paths = [ env['CM_TMP_CURRENT_SCRIPT_PATH'] ]
 
     for inc_path in include_paths:
         env['+C_INCLUDE_PATH'].append(inc_path)
         env['+CPLUS_INCLUDE_PATH'].append(inc_path)
 
-    lib_path = os.path.join(os.getcwd())
-    env['+LD_LIBRARY_PATH'].append(lib_path)
-    env['+DYLD_FALLBACK_LIBRARY_PATH'].append(lib_path)
+    version = "master"
+    env['CM_QAIC_API_SRC_FILE'] = os.path.join(env['CM_TMP_CURRENT_SCRIPT_PATH'], version, "QAicInfApi.cpp")
+    env['CM_QAIC_API_INC_FILE'] = os.path.join(env['CM_TMP_CURRENT_SCRIPT_PATH'], version, "QAicInfApi.h")
 
     return {'return':0}
