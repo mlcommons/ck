54cf4a57737c3b41b3be743acaca39ba8cfe4146
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-12-01 18:35:33 +0530

    Fixes for stable diffusion

--- cm-mlops/script/get-ml-model-stable-diffusion/customize.py
+++ cm-mlops/script/get-ml-model-stable-diffusion/customize.py
@@ -6,7 +6,7 @@ def preprocess(i):
     os_info = i['os_info']
     env = i['env']
 
-    path = env.get('GPTJ_CHECKPOINT_PATH', '').strip()
+    path = env.get('SDXL_CHECKPOINT_PATH', '').strip()
 
     if path == '' or not os.path.exists(path):
         env['CM_TMP_REQUIRE_DOWNLOAD'] = 'yes'
@@ -17,9 +17,8 @@ def postprocess(i):
 
     env = i['env']
 
-    env['GPTJ_CHECKPOINT_PATH'] = os.path.join(env['GPTJ_CHECKPOINT_PATH'], "checkpoint-final")
-    env['CM_ML_MODEL_FILE_WITH_PATH'] = env['GPTJ_CHECKPOINT_PATH']
-    env['CM_ML_MODEL_FILE'] = os.path.basename(env['CM_ML_MODEL_FILE_WITH_PATH'])
-    env['CM_GET_DEPENDENT_CACHED_PATH'] = env['CM_ML_MODEL_FILE_WITH_PATH']
+    env['SDXL_CHECKPOINT_PATH'] = env['CM_ML_MODEL_PATH']
+    env['CM_ML_MODEL_PATH'] = env['SDXL_CHECKPOINT_PATH']
+    env['CM_GET_DEPENDENT_CACHED_PATH'] = env['CM_ML_MODEL_PATH']
 
     return {'return':0}
