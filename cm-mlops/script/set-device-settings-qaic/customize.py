ea55e7876d5bfa0787404b6689deea72b16444ec
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-12-15 15:36:50 +0000

    Fix qaic device setting

--- cm-mlops/script/set-device-settings-qaic/customize.py
+++ cm-mlops/script/set-device-settings-qaic/customize.py
@@ -18,12 +18,12 @@ def preprocess(i):
         for device in env['CM_QAIC_DEVICES'].split(","):
             ecc_template = {}
             ecc_template['request'] = []
-            ecc_template[0] = {}
-            ecc_template[0]['qid'] = device
-            ecc_template[0]['dev_config'] = {}
-            ecc_template[0]['dev_config']['update_ras_ecc_config_request'] = {}
-            ecc_template[0]['dev_config']['update_ras_ecc_config_request']['ras_ecc'] = []
-            ecc_template[0]['dev_config']['update_ras_ecc_config_request']['ras_ecc'].append("RAS_DDR_ECC")
+            ecc_template['request'][0] = {}
+            ecc_template['request'][0]['qid'] = device
+            ecc_template['request'][0]['dev_config'] = {}
+            ecc_template['request'][0]['dev_config']['update_ras_ecc_config_request'] = {}
+            ecc_template['request'][0]['dev_config']['update_ras_ecc_config_request']['ras_ecc'] = []
+            ecc_template['request'][0]['dev_config']['update_ras_ecc_config_request']['ras_ecc'].append("RAS_DDR_ECC")
             with open("request_"+device+".json", "w") as f:
                 f.write(json.dumps(ecc_template))
 
