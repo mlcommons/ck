d572c49c99fdc63db071512fcd36b7c58b2ba916
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-12-30 21:31:35 +0000

    Fix an issue with generic-python-lib recursive installation

--- cm-mlops/script/get-generic-python-lib/uninstall_deps.sh
+++ cm-mlops/script/get-generic-python-lib/uninstall_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -n ${CM_GENERIC_PYTHON_PIP_UNINSTALL_DEPS} ]]; then
-    cmd="${CM_PYTHON_BIN_WITH_PATH} -m pip uninstall ${CM_GENERIC_PYTHON_PIP_UNINSTALL_DEPS} -y"
+    cmd="${CM_PYTHON_BIN_WITH_PATH} -m pip uninstall ${CM_GENERIC_PYTHON_PIP_UNINSTALL_DEPS} -y ${CM_PYTHON_PIP_COMMON_EXTRA}"
     echo "$cmd"
     eval "$cmd"
     test $? -eq 0 || exit $?
