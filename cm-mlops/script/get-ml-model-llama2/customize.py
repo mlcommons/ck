7f75cd2f98c2f3b3c3dc23f864b7c98d0a4ac964
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-01-04 02:25:28 +0530

    Fix LLAMA2 naming

--- cm-mlops/script/get-ml-model-llama2/customize.py
+++ cm-mlops/script/get-ml-model-llama2/customize.py
@@ -6,7 +6,7 @@ def preprocess(i):
     os_info = i['os_info']
     env = i['env']
 
-    path = env.get('SDXL_CHECKPOINT_PATH', '').strip()
+    path = env.get('LLAMA2_CHECKPOINT_PATH', '').strip()
 
     if path == '' or not os.path.exists(path):
         env['CM_TMP_REQUIRE_DOWNLOAD'] = 'yes'
@@ -17,8 +17,8 @@ def postprocess(i):
 
     env = i['env']
 
-    env['SDXL_CHECKPOINT_PATH'] = env['CM_ML_MODEL_PATH']
-    env['CM_ML_MODEL_PATH'] = env['SDXL_CHECKPOINT_PATH']
+    env['LLAMA2_CHECKPOINT_PATH'] = env['CM_ML_MODEL_PATH']
+    env['CM_ML_MODEL_PATH'] = env['LLAMA2_CHECKPOINT_PATH']
     env['CM_GET_DEPENDENT_CACHED_PATH'] = env['CM_ML_MODEL_PATH']
 
     return {'return':0}
