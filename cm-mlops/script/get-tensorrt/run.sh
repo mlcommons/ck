7ceafc9f555b9cce382fb63118e83e38b2e06174
Arjun <arjunsuresh1987@gmail.com>
2024-01-23 21:16:23 +0530

    Support cuda 12.3.2, tensorrt install on ubuntu23.03

--- cm-mlops/script/get-tensorrt/run.sh
+++ cm-mlops/script/get-tensorrt/run.sh
@@ -4,6 +4,8 @@ if [[ ${CM_TENSORRT_VERSION} == 'vdetected' ]]; then
   exit 0;
 fi
 
+PIP_EXTRA=`python3 -c "import pkg_resources; print(' --break-system-packages ' if int(pkg_resources.get_distribution('pip').version.split('.')[0]) >= 23 else '')"`
+
 version=${CM_TENSORRT_VERSION}
 install_dir=${CM_TENSORRT_INSTALL_PATH}
 python_version=${CM_PYTHON_VERSION}
@@ -12,19 +14,19 @@ python_max_version=${python_version_info[0]}
 python_min_version=${python_version_info[1]}
 
 cd ${install_dir}/python
-${CM_PYTHON_BIN_WITH_PATH} -m pip install tensorrt-*-cp${python_max_version}${python_min_version}-none-${CM_HOST_OS_TYPE}_${CM_HOST_OS_MACHINE}.whl
+${CM_PYTHON_BIN_WITH_PATH} -m pip install tensorrt-*-cp${python_max_version}${python_min_version}-none-${CM_HOST_OS_TYPE}_${CM_HOST_OS_MACHINE}.whl $PIP_EXTRA
 test $? -eq 0 || exit $?
 
 cd ${install_dir}/uff
-${CM_PYTHON_BIN_WITH_PATH} -m pip install uff-0.6.9-py2.py3-none-any.whl
+${CM_PYTHON_BIN_WITH_PATH} -m pip install uff-0.6.9-py2.py3-none-any.whl $PIP_EXTRA
 test $? -eq 0 || exit $?
 
 cd ${install_dir}/graphsurgeon
-${CM_PYTHON_BIN_WITH_PATH} -m pip install graphsurgeon-0.4.6-py2.py3-none-any.whl
+${CM_PYTHON_BIN_WITH_PATH} -m pip install graphsurgeon-0.4.6-py2.py3-none-any.whl $PIP_EXTRA
 test $? -eq 0 || exit $?
 
 cd ${install_dir}/onnx_graphsurgeon
-${CM_PYTHON_BIN_WITH_PATH} -m pip install onnx_graphsurgeon-0.3.12-py2.py3-none-any.whl
+${CM_PYTHON_BIN_WITH_PATH} -m pip install onnx_graphsurgeon-0.3.12-py2.py3-none-any.whl $PIP_EXTRA
 test $? -eq 0 || exit $?
 
 #create softlinks for libnvinfer.so.7 and libnvinfer_plugin.so.7
