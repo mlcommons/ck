679cff41c40aca668516024ea4a1835eb43c819b
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-01-26 04:40:55 +0530

    Fixes for onnxruntime install from src

--- cm-mlops/script/install-onnxruntime-from.src/run.sh
+++ cm-mlops/script/install-onnxruntime-from.src/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+export CC=${CM_GCC_BIN_WITH_PATH}
+export CXX=${CM_GCC_INSTALLED_PATH}/g++
+
 echo "cd ${CM_RUN_DIR}"
 cd ${CM_RUN_DIR}
 test $? -eq 0 || exit $?
-echo ${CM_RUN_CMD}
+rm -rf build
 
+echo ${CM_RUN_CMD}
 eval ${CM_RUN_CMD}
 test $? -eq 0 || exit $?
 
