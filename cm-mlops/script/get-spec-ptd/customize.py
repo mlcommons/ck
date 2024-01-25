34bbba86dadd538ee7a3b48de91b4b1f2b6bd818
Arjun <arjunsuresh1987@gmail.com>
2024-01-25 00:42:11 +0000

    Support mlperf power server on windows

--- cm-mlops/script/get-spec-ptd/customize.py
+++ cm-mlops/script/get-spec-ptd/customize.py
@@ -6,27 +6,6 @@ def preprocess(i):
 
     os_info = i['os_info']
 
-    if os_info['platform'] == 'windows':
-        return {'return':1, 'error': 'Windows is not supported in this script yet'}
-
-    env = i['env']
-    meta = i['meta']
-
-    if env.get('CM_INPUT') and not env.get('CM_MLPERF_PTD_PATH'):
-        env['CM_MLPERF_PTD_PATH'] = env['CM_INPUT']
-
-    if 'CM_GIT_DEPTH' not in env:
-        env['CM_GIT_DEPTH'] = ''
-
-    if 'CM_GIT_RECURSE_SUBMODULES' not in env:
-        env['CM_GIT_RECURSE_SUBMODULES'] = ''
-
-    need_version = env.get('CM_VERSION','')
-    versions = meta['versions']
-
-    if need_version!='' and not need_version in versions:
-        env['CM_GIT_CHECKOUT'] = need_version
-
     return {'return':0}
 
 
@@ -36,7 +15,7 @@ def postprocess(i):
     state = i['state']
 
     if 'CM_MLPERF_PTD_PATH' not in env:
-        env['CM_MLPERF_PTD_PATH'] = os.path.join(os.getcwd(), 'power', 'inference_v1.0', 'ptd-linux-x86')
+        env['CM_MLPERF_PTD_PATH'] = os.path.join(env['CM_MLPERF_POWER_SOURCE'], 'inference_v1.0', 'ptd-linux-x86')
     env['CM_SPEC_PTD_PATH'] = env['CM_MLPERF_PTD_PATH']
 
     return {'return':0}
