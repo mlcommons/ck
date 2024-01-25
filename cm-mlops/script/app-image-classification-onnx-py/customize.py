b1b5288ba368f89cdedbc947437f825df1e39a20
Grigori Fursin <Grigori.Fursin@cTuning.org>
2024-01-25 18:12:11 +0100

    clean up of demo

--- cm-mlops/script/app-image-classification-onnx-py/customize.py
+++ cm-mlops/script/app-image-classification-onnx-py/customize.py
@@ -27,16 +27,28 @@ def postprocess(i):
 
     data = state.get('cm_app_image_classification_onnx_py',{})
 
+    fjson = 'cm-image-classification-onnx-py.json'
+    fyaml = 'cm-image-classification-onnx-py.yaml'
+
+    output=env.get('CM_APP_IMAGE_CLASSIFICATION_ONNX_PY_OUTPUT','')
+    if output!='':
+        if not os.path.exists(output):
+            os.makedirs(output)
+
+        fjson=os.path.join(output, fjson)
+        fyaml=os.path.join(output, fyaml)
+
     try:
         import json
-        with open('cm-image-classification-onnx-py.json', 'w', encoding='utf-8') as f:
+        with open(fjson, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
     except Exception as e:
         print ('CM warning: {}'.format(e))
 
+    
     try:
         import yaml
-        with open('cm-image-classification-onnx-py.yaml', 'w', encoding='utf-8') as f:
+        with open(fyaml, 'w', encoding='utf-8') as f:
             yaml.dump(data, f)
     except Exception as e:
         print ('CM warning: {}'.format(e))
