8e777d2e45e06c2461ff3d6bf77a9e567a0f3c19
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-01-29 21:48:02 +0530

    Refactor intel-gptj code, (WIP), added force_new_env_keys option

--- cm-mlops/script/get-ml-model-gptj/run-intel.sh
+++ cm-mlops/script/get-ml-model-gptj/run-intel.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-echo ${CM_HARNESS_CODE_ROOT}
-cd ${CM_HARNESS_CODE_ROOT}
-
+export PATH=${CM_CONDA_BIN_PATH}:$PATH
 
 export CALIBRATION_DATA_JSON=${CM_CALIBRATION_DATASET_CNNDM_PATH}
-export VALIDATION_DATA_JSON=${CM_DATASET_CNNDM_EVAL_PATH}
-export INT4_CALIBRATION_DIR=${WORKLOAD_DATA}/quantized-int4-model
-#sudo -E bash run_quantization.sh
-bash run_quantization.sh
+
+
+if [[ ${CM_ML_MODEL_WEIGHT_DATA_TYPES} == "int4" ]]; then
+  export INT4_CALIBRATION_DIR=${PWD}/quantized-int4-model
+  bash ${CM_TMP_CURRENT_SCRIPT_PATH}/run-int4-calibration.sh
+  cd ${CM_HARNESS_CODE_ROOT}
+  bash run_quantization_int4.sh
+else
+  cd ${CM_HARNESS_CODE_ROOT}
+  bash run_quantization.sh
+fi
+
+test $? -eq 0 || exit $?
