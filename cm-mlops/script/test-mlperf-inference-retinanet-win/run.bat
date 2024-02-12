7b1ef52d98d4af8a0651433b87007df7b9ed1b96
Grigori Fursin <Grigori.Fursin@cTuning.org>
2024-02-12 14:56:50 +0100

    clean up

--- cm-mlops/script/test-mlperf-inference-retinanet-win/run.bat
+++
@@ -1,8 +0,0 @@
-echo.
-
-set CUR_DIR=%cd%
-set SCRIPT_DIR=%CM_TMP_CURRENT_SCRIPT_PATH%
-
-cd %CM_MLPERF_INFERENCE_CLASSIFICATION_AND_DETECTION_PATH%
-
-%CM_PYTHON_BIN_WITH_PATH% python/main.py --profile retinanet-onnxruntime --scenario Offline --model %CM_ML_MODEL_FILE_WITH_PATH% --dataset-path %CM_DATASET_PATH_ROOT%\validation\data --accuracy
--- cm-mlops/script/test-mlperf-inference-retinanet-win/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo ""
-
-cd ${CM_MLPERF_INFERENCE_CLASSIFICATION_AND_DETECTION_PATH}
-
-${CM_PYTHON_BIN_WITH_PATH} python/main.py --profile retinanet-onnxruntime --scenario Offline --model ${CM_ML_MODEL_FILE_WITH_PATH} --dataset-path ${CM_DATASET_PATH_ROOT} --accuracy
