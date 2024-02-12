d1e78034baf40cb6cfdb3e8fbc7841631bd035e0
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-02-12 20:35:27 +0000

    Improve the measurement readme generation

--- cm-mlops/script/generate-mlperf-inference-submission/customize.py
+++ cm-mlops/script/generate-mlperf-inference-submission/customize.py
@@ -172,6 +172,7 @@ def generate_submission(i):
                         scenarios.append("multistream")
 
                 modes = [f for f in os.listdir(result_scenario_path) if not os.path.isfile(os.path.join(result_scenario_path, f))]
+                power_run = False
                 for mode in modes:
                     result_mode_path = os.path.join(result_scenario_path, mode)
                     submission_mode_path = os.path.join(submission_scenario_path, mode)
@@ -188,7 +189,6 @@ def generate_submission(i):
                         os.makedirs(submission_measurement_path)
 
                     if mode=='performance':
-                        power_run = False
 
                         if os.path.exists(os.path.join(result_mode_path, "power")):
                             power_run = True
@@ -262,10 +262,7 @@ def generate_submission(i):
                                 files.append(f)
                             elif f == "spl.txt":
                                 files.append(f)
-                            elif f == "README.md":
-                                readme = True
-                                shutil.copy(os.path.join(result_mode_path, f), os.path.join(submission_measurement_path, f))
-                            elif f in [ "version_info.json", "os_info.json", "cpu_info.json", "pip_freeze.json" ] and mode == "performance":
+                            elif f in [ "README.md", "version_info.json", "os_info.json", "cpu_info.json", "pip_freeze.json" ] and mode == "performance":
                                 shutil.copy(os.path.join(result_mode_path, f), os.path.join(submission_measurement_path, f))
                             elif f in [ "console.out" ]:
                                 shutil.copy(os.path.join(result_mode_path, f), os.path.join(submission_measurement_path, mode+"_"+f))
@@ -282,12 +279,122 @@ def generate_submission(i):
                         p_target = os.path.join(submission_results_path, f)
                         shutil.copy(os.path.join(result_mode_path, f), p_target)
 
-                    if not readme and mode in [ "performance", "accuracy" ]:
-                        with open(os.path.join(submission_measurement_path, "README.md"), mode='w') as f:
-                            f.write("TBD") #create an empty README
+                readme_file = os.path.join(submission_measurement_path, "README.md")
+                if not os.path.exists(readme_file):
+                    with open(readme_file, mode='w') as f:
+                        f.write("TBD") #create an empty README
+                else:
+                    readme_suffix = ""
+                    result_string = get_result_string(env['CM_MLPERF_LAST_RELEASE'], model, scenario, result_scenario_path, power_run)
+                    with open(readme_file, mode='a') as f:
+                        f.write(result_string)
+
 
     return {'return':0}
 
+
+def get_accuracy_metric(config, model, path):
+
+    import submission_checker as checker
+    import re
+    is_valid = False
+    all_accuracy_valid = True
+    acc = None
+    result_acc = None
+    target = config.get_accuracy_target(model)
+    acc_upper_limit = config.get_accuracy_upper_limit(model)
+    patterns = []
+    acc_targets = []
+    acc_limits = []
+    up_patterns = []
+    acc_types = []
+
+    if acc_upper_limit is not None:
+        acc_limit_check = True
+        for i in range(0, len(acc_upper_limit), 2):
+            acc_type, acc_target = acc_upper_limit[i:i+2]
+            acc_limits.append(acc_target)
+            up_patterns.append(checker.ACC_PATTERN[acc_type])
+
+    for i in range(0, len(target), 2):
+        acc_type, acc_target = target[i:i+2]
+        acc_types.append(acc_type)
+        patterns.append(checker.ACC_PATTERN[acc_type])
+        acc_targets.append(acc_target)
+
+    acc_seen = [False for _ in acc_targets]
+    acc_results = {}
+    with open(os.path.join(path, "accuracy.txt"), "r", encoding="utf-8") as f:
+        for line in f:
+            for i, (pattern, acc_target, acc_type) in enumerate(zip(patterns, acc_targets, acc_types)):
+                m = re.match(pattern, line)
+                if m:
+                    acc = m.group(1)
+
+                    acc_results[acc_type] = acc
+
+                if acc is not None and float(acc) >= acc_target:
+                    all_accuracy_valid &= True
+                    acc_seen[i] = True
+                elif acc is not None:
+                    all_accuracy_valid = False
+                    #log.warning("%s accuracy not met: expected=%f, found=%s", path, acc_target, acc)
+                if i == 0 and acc:
+                    result_acc = acc
+                acc = None
+            if acc_upper_limit is not None:
+                for i, (pattern, acc_limit) in enumerate(zip(up_patterns, acc_limits)):
+                    m = re.match(pattern, line)
+                    if m:
+                        acc = m.group(1)
+                    if acc is not None and acc_upper_limit is not None and float(acc) > acc_limit:
+                        acc_limit_check = False
+                        #log.warning("%s accuracy not met: upper limit=%f, found=%s", path, acc_limit, acc)
+                    acc = None
+            if all(acc_seen):
+                break;
+        is_valid = all_accuracy_valid & all(acc_seen)
+        if acc_upper_limit is not None:
+            is_valid &= acc_limit_check
+
+
+    return acc_results, acc_targets, acc_limits, up_patterns
+
+
+def get_result_string(version, model, scenario, result_path, has_power):
+    import submission_checker as checker
+    config = checker.Config(
+        version,
+        None,
+        ignore_uncommited=False,
+        skip_power_check=False,
+    )
+    mlperf_model = config.get_mlperf_model(model)
+    performance_path = os.path.join(result_path, "performance", "run_1")
+    accuracy_path = os.path.join(result_path, "accuracy")
+    performance_result = checker.get_performance_metric(config, mlperf_model, performance_path, scenario, None, None, has_power)
+    if has_power:
+        is_valid, power_metric, scenario, avg_power_efficiency = checker.get_power_metric(config, scenario, performance_path, True, performance_result)
+        power_result_string = power_metric
+    acc_results, acc_targets, acc_limits, up_patterns = get_accuracy_metric(config, mlperf_model, accuracy_path)
+
+    result_field = checker.RESULT_FIELD[checker.SCENARIO_MAPPING[scenario]]
+
+    performance_result_string = f"{result_field}: {performance_result}\n"
+    accuracy_result_string = ''
+    for i, acc in enumerate(acc_results):
+        accuracy_result_string += f"{acc}: {acc_results[acc]}"
+        if not up_patterns:
+            accuracy_result_string += f", Required accuracy for closed division >= {round(acc_targets[i], 5)}"
+
+    result_string = "\n\n## Results \n"
+    result_string += "### Accuracy Results \n" + accuracy_result_string
+    result_string += "\n\n### Performance Results \n" + performance_result_string
+    if has_power:
+        result_string += "\n\n### Power Results \n" + power_result_string
+
+    return result_string
+ 
 def postprocess(i):
 
     r = generate_submission(i)
