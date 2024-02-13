a164816a3dab834f8f412a11d200acfb37590128
Grigori Fursin <Grigori.Fursin@cTuning.org>
2024-02-13 16:05:24 +0100

    added support for git patching on windows

--- cm-mlops/script/get-git-repo/run.bat
+++ cm-mlops/script/get-git-repo/run.bat
@@ -48,24 +48,11 @@ if not "%CM_GIT_SUBMODULES%" == "" (
 )
 
 if "%CM_GIT_PATCH%" == "yes" (
-   echo Git patching is not yet implemented in CM script "get-git-repo" - please add it!
-   pause
-
-   rem  set patch_filename=%CM_GIT_PATCH_FILENAME%
-   rem  if [ ! -n ${CM_GIT_PATCH_FILENAMES} ]; then
-   rem    patchfile=${CM_GIT_PATCH_FILENAME:-"git.patch"}
-   rem    CM_GIT_PATCH_FILENAMES=$patchfile
-   rem  fi
-   rem
-   rem  IFS=', ' read -r -a patch_files <<< ${CM_GIT_PATCH_FILENAMES}
-   rem
-   rem  for patch_filename in "${patch_files[@]}"
-   rem  do
-   rem    echo "Applying patch ${SCRIPT_DIR}/patch/$patch_filename"
-   rem    git apply ${SCRIPT_DIR}/patch/"$patch_filename"
-   rem    if [ "${?}" != "0" ]; then exit 1; fi
-   rem  done
-
+   for %%x in (%CM_GIT_PATCH_FILEPATHS%) do (
+       echo Applying patch %%x ...
+       git apply %%x
+       IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
+   )
 )
 
 cd %CUR_DIR%
