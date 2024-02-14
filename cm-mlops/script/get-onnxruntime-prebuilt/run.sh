69c121142baddcbc175bd171a6963010429b9cdc
Grigori Fursin <Grigori.Fursin@cTuning.org>
2024-02-14 19:54:24 +0100

    added ONNX C++ prebuilt download on Windows

--- cm-mlops/script/get-onnxruntime-prebuilt/run.sh
+++ cm-mlops/script/get-onnxruntime-prebuilt/run.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
-CM_VERSION=${CM_VERSION:-1.12.1}
+rm -rf install
+rm -f ${FILENAME}
 
 mkdir -p install
-machine=${CM_HOST_OS_MACHINE:-x86_64}
-if [[ $machine == "x86_64" ]]; then
-  machine=x64
-fi
-hostos=${CM_HOST_OS_TYPE}
-if [[ $hostos == "darwin" ]]; then
-  hostos=osx
-fi
-device=${CM_ONNXRUNTIME_DEVICE}
-if [[ $device != "" ]]; then
-  machine=$machine-$device
-fi
 
-FOLDER=onnxruntime-${hostos}-$machine-${CM_VERSION}
-FILENAME=$FOLDER".tgz"
-WGET_CMD="-q --no-check-certificate https://github.com/microsoft/onnxruntime/releases/download/v${CM_VERSION}/${FILENAME}"
-echo "wget ${WGET_CMD}"
-wget ${WGET_CMD}
+wget --no-check-certificate ${URL}
 test $? -eq 0 || exit 1
+
 tar -C install -xzf ${FILENAME}
 test $? -eq 0 || exit 1
-echo "CM_TMP_INSTALL_FOLDER=$FOLDER" > tmp-run-env.out
 
+echo "CM_TMP_INSTALL_FOLDER=$FOLDER" > tmp-run-env.out
