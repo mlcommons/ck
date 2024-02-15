58251a66603a2045750eba2c624f60a4185d211e
Grigori Fursin <Grigori.Fursin@cTuning.org>
2024-02-15 22:38:37 +0100

    demos

--- cm-mlops/script/get-ml-model-huggingface-zoo/download_model.py
+++ cm-mlops/script/get-ml-model-huggingface-zoo/download_model.py
@@ -19,18 +19,18 @@ if model_task == "prune":
         f.write(f"CM_ML_MODEL_FILE_WITH_PATH={os.path.join(os.getcwd(),'')}")
 
 else:
-        model_filename = os.environ.get('CM_MODEL_ZOO_FILENAME', '')
-        if model_filename == '': 
-            model_filename = 'model.onnx'
-
         subfolder = os.environ.get('CM_HF_SUBFOLDER', '')
+        full_subfolder = os.environ.get('CM_HF_FULL_SUBFOLDER', '')
+
+        model_filename = os.environ.get('CM_MODEL_ZOO_FILENAME', '')
+        if model_filename == '':
+            model_filename = 'model.onnx'
 
         model_filenames = model_filename.split(',') if ',' in model_filename else [model_filename]
 
         # First must be model
         base_model_filename = model_filenames[0]
 
-        full_subfolder = os.environ.get('CM_HF_FULL_SUBFOLDER', '')
         files = []
         if full_subfolder!='':
 
@@ -43,17 +43,40 @@ else:
             print ('')
             print ('Listing files in {} ...'.format(path))
 
-            files=fs.ls(path, detail=False)            
+            def list_hf_files(path):
+                all_files = []
+                
+                files=fs.ls(path, revision=revision) #, detail=False)            
+
+                for f in files:
+                    fname = f['name']
+                    fdir = f['type'] == 'directory'
+
+                    if fdir:
+                        all_files += list_hf_files(fname)
+                    else:
+                        all_files.append(fname)
+
+                return all_files
+                        
+            
+            files=list_hf_files(path)
 
             print ('')
             print ('Found {} files'.format(len(files)))
             
             for f in files:
-                ff = f[len(model_stub)+1:]
+                print (f)
+
+                remove = len(model_stub)+1
+
+                if revision!='':
+                    remove+=len(revision)+1
+
+                ff = f[remove:]
 
                 if ff not in model_filenames:
                     model_filenames.append(ff)
-
 
 
         print ('')
