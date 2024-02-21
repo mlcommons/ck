df36b87028299801275e4b18c3b3e8039e79e268
Arjun <arjunsuresh1987@gmail.com>
2024-02-21 11:34:38 +0000

    Fix break-system-packages

--- cm-mlops/script/install-pip-package-for-cmind-python/customize.py
+++ cm-mlops/script/install-pip-package-for-cmind-python/customize.py
@@ -5,22 +5,27 @@ import sys
 
 def install(package):
     additional_install_options = ""
-    r = subprocess.run([sys.executable, "-m", "pip", "--version"], check=True)
-    out_split = r.split(" ")
-    if len(out_split) < 2:
-        return {'return': 1, 'error': 'Pip version detection failed'}
-    pip_version = r[1].split(".")
-    if pip_version and len(pip_version) > 1 and int(pip_version[0]) >= 23:
-        additional_install_options += " --break-system-packages"
-
-    subprocess.run([sys.executable, "-m", "pip", "install", package, additional_install_options], check=True)
+    r = subprocess.run([sys.executable, "-m", "pip", "--version"], check=True, capture_output=True)
+    r = r.stdout.decode("utf-8")
+    if "pip" in r:
+        out_split = r.split(" ")
+        if len(out_split) < 2:
+            return {'return': 1, 'error': 'Pip version detection failed'}
+        pip_version = out_split[1].split(".")
+        if pip_version and len(pip_version) > 1 and int(pip_version[0]) >= 23:
+            additional_install_options += " --break-system-packages"
+    r = subprocess.run([sys.executable, "-m", "pip", "install", package, additional_install_options], check=True)
+ 
+    return {'return':0}
 
 def preprocess(i):
 
     os_info = i['os_info']
     env = i['env']
     if env.get('CM_PIP_PACKAGE_NAME', '') != '':
-        install(env['CM_PIP_PACKAGE_NAME'])
+        r = install(env['CM_PIP_PACKAGE_NAME'])
+        if r['return'] > 0:
+            return r
 
     return {'return':0}
 
