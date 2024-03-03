75a7551f54a38f683fcaf1fcfe76abf6ecac9b20
Grigori Fursin <gfursin@gmail.com>
2024-03-03 22:16:06 +0000

    changing outdated pkg_resources to importlib.metadata to support Python 3.12+

--- cm-mlops/script/app-image-classification-onnx-py/run.sh
+++ cm-mlops/script/app-image-classification-onnx-py/run.sh
@@ -23,7 +23,7 @@ export CK_BATCH_COUNT=${CM_BATCH_COUNT}
 
 if [[ "${CM_INPUT}" != "" ]]; then export CM_IMAGE=${CM_INPUT}; fi
 
-PIP_EXTRA=`${CM_PYTHON_BIN} -c "import pkg_resources; print(' --break-system-packages ' if int(pkg_resources.get_distribution('pip').version.split('.')[0]) >= 23 else '')"`
+PIP_EXTRA=`${CM_PYTHON_BIN} -c "import importlib.metadata; print(' --break-system-packages ' if int(importlib.metadata.version('pip').split('.')[0]) >= 23 else '')"`
 
 echo ""
 ${CM_PYTHON_BIN} -m pip install -r ${CM_TMP_CURRENT_SCRIPT_PATH}/requirements.txt ${PIP_EXTRA}
