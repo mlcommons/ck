84be39d0aa3423b5ca3f91a684855f93130080e8
Arjun <arjunsuresh1987@gmail.com>
2024-03-14 20:56:40 +0000

    Fixes for intel gptj int4 calibration

--- cm-mlops/script/app-mlperf-inference-intel/calibrate_gptj_int4_model.sh
+++ cm-mlops/script/app-mlperf-inference-intel/calibrate_gptj_int4_model.sh
@@ -18,7 +18,20 @@ echo $cmd
 eval $cmd
 
 export QUANTIZED_MODEL_DIR=${WORKLOAD_DATA}/quantized-int4-model
-
 mkdir -p ${QUANTIZED_MODEL_DIR}
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+CONDA_INSTALL_PATH=`pwd`/miniconda3
+rm -rf ${CONDA_INSTALL_PATH}
+bash miniconda.sh -b -p ${CONDA_INSTALL_PATH}
+export CONDA_PREFIX=${CONDA_INSTALL_PATH}
+
+export PATH=${CONDA_INSTALL_PATH}/bin:$PATH
+conda install -y python=3.9.0 numpy=1.23.5
+python -m pip install transformers==4.21.2
+python -m pip install texttable
+python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+python -m pip install datasets
 bash run_calibration_int4.sh
 test $? -eq 0 || exit $?
+#exit 1
