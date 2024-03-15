f3945d45366df68b8be774c4545ee91eb23c8fbe
Arjun <arjunsuresh1987@gmail.com>
2024-03-15 12:54:15 +0000

    Support SUT configs for intel-mlperf-inference

--- cm-mlops/script/app-mlperf-inference-intel/run_gptj_harness.sh
+++ cm-mlops/script/app-mlperf-inference-intel/run_gptj_harness.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=${CM_CONDA_BIN_PATH}:$PATH
 
-export KMP_BLOCKTIME=10
+export KMP_BLOCKTIME=${KMP_BLOCKTIME}
 export KMP_AFFINITY=granularity=fine,compact,1,0
 export LD_PRELOAD=${LD_PRELOAD}:${CONDA_PREFIX}/lib/libiomp5.so
 export LD_PRELOAD=${LD_PRELOAD}:${CONDA_PREFIX}/lib/libtcmalloc.so
@@ -9,11 +9,11 @@ export LD_PRELOAD=${LD_PRELOAD}:${CONDA_PREFIX}/lib/libtcmalloc.so
 export num_physical_cores=`lscpu -b -p=Core,Socket | grep -v '^#' | sort -u | wc -l`
 num_numa=$(numactl --hardware|grep available|awk -F' ' '{ print $2 }')
 
-NUM_PROC=$num_numa
+NUM_PROC=${NUM_PROC:-$num_numa}
 CPUS_PER_PROC=$((num_physical_cores/num_numa))
-WORKERS_PER_PROC=1
+WORKERS_PER_PROC=3
 TOTAL_SAMPLE_COUNT=13368
-BATCH_SIZE=8
+BATCH_SIZE=${CM_MLPERF_LOADGEN_BATCH_SIZE}
 TIMESTAMP=$(date +%m-%d-%H-%M)
 HOSTNAME=$(hostname)
 #OUTPUT_DIR=offline-output-${HOSTNAME}-batch-${BATCH_SIZE}-procs-${NUM_PROC}-ins-per-proc-${WORKERS_PER_PROC}-${TIMESTAMP}
@@ -27,21 +27,20 @@ OUTPUT_DIR="${CM_MLPERF_OUTPUT_DIR}"
 USER_CONF="${CM_MLPERF_USER_CONF}"
 
 
-	#--mode Performance \
 cmd="python runner.py --workload-name gptj \
 	--scenario Offline \
 	--mode ${LOADGEN_MODE} \
 	--num-proc ${NUM_PROC} \
 	--cpus-per-proc ${CPUS_PER_PROC} \
 	--model-checkpoint-path ${CHECKPOINT_DIR} \
-	--warmup \
+	${WARMUP} \
 	--dataset-path ${VALIDATION_DATA_JSON} \
 	--batch-size ${BATCH_SIZE} \
 	--mlperf-conf ${CM_MLPERF_CONF} \
 	--user-conf ${CM_MLPERF_USER_CONF} \
-	--precision int8 \
+	--precision ${PRECISION} \
 	--pad-inputs \
-	--quantized-model ${INT8_MODEL_DIR}/best_model.pt \
+	--quantized-model ${QUANTIZED_MODEL}  \
 	--workers-per-proc ${WORKERS_PER_PROC} \
 	--total-sample-count ${TOTAL_SAMPLE_COUNT} \
 	--output-dir ${OUTPUT_DIR} \
