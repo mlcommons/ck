1ae141e7b5e2aad248f8f32a7bc2c83869212fd6
Grigori Fursin <Grigori.Fursin@cTuning.org>
2024-04-03 11:41:52 +0200

    clean up

--- cm-mlops/script/test-mlperf-loadgen-py/python/main.py
+++ cm-mlops/script/test-mlperf-loadgen-py/python/main.py
@@ -4,11 +4,19 @@ Testing MLPerf loadgen
 
 import numpy as np
 
+print ('')
 print ('- Importing MLPerf loadgen library ...')
 import mlperf_loadgen as lg
 print ('  SUCCESS!')
 
+print ('')
 print ('- Importing CM library ...')
 import cmind as cm
 print ('  SUCCESS!')
 
+print ('')
+print ('- List CM repos ...')
+print ('')
+r = cm.access({'action':'ls', 'automation':'repo', 'out':'con'})
+print ('')
+print ('  SUCCESS!')
